1b2129ffe864f5706651c75fd3b4d52748d1e66d
Hyerim Yang <hlhlstar@naver.com>
2025-01-13 15:12:34 +0900

    chore: Eslint, Prettier, Tailwind CSS 등 설치 및 초기 설정
    - Tailwind CSS 설치 및 PostCSS와 Autoprefixer 연동
    - Eslint와 Prettier 통합 설정 완료

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,93 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: [
+      "dist/",
+      "build/",
+      "node_modules/",
+      ".eslintrc.cjs",
+      "npm-debug.log",
+      "yarn-debug.log",
+      "yarn-error.log",
+      "public/",
+      "coverage/",
+      ".vscode/",
+      ".idea/",
+      ".DS_Store",
+      // "*.config.js",
+      "package-lock.json",
+    ], },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        React: 'readonly',
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    settings: {
+      react: {
+        version: '18.2',
+        runtime: 'automatic'
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.css']
+        }
+      },
+      tailwindcss: {
+        config: './tailwind.config.js',
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettier,
+      'react': react,
+      'jsx-a11y': jsxA11y,
+      'import': importPlugin,
+      'tailwindcss': tailwindcss
     },
     rules: {
+      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+      'varsIgnorePattern': '^_',
+      'argsIgnorePattern': '^_',
+      'ignoreRestSiblings': true
+      }],
+      'eqeqeq': ['error', 'always'],
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off'
+    }
+  }
 )
