c3ddf92d2b1356bce2f76367d4d222046d3926d5
kwonboryong <kwonboryong@naver.com>
2025-01-14 14:54:22 +0900

    feat: Input 컴포넌트 스타일 추가 및 variant prop을 추가하여 스타일 변형 지원

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,31 @@
-const Input = () => {
-  return <div></div>;
+import { Input } from './ui/input';
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  type: string;
+  placeholder: string;
+  variant?: 'default' | 'projectName';
+}
+
+const InputComponent = ({
+  variant = 'default',
+  type,
+  placeholder,
+}: InputProps) => {
+  const variantStyles = {
+    default: 'w-[290px]',
+    projectName: 'w-[500px]',
+  };
+
+  return (
+    <>
+      <Input
+        type={type}
+        placeholder={placeholder}
+        className={`h-[40px] ${variantStyles[variant]}`}
+      />
+    </>
+  );
 };
 
-export default Input;
+export default InputComponent;
