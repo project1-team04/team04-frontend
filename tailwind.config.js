fe6d885704c124b920eda3e94a982f71c125dd11
Hyerim Yang <hlhlstar@naver.com>
2025-01-14 20:05:41 +0900

    refactor: 색상 및 배경 구조 그룹화

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,27 +11,33 @@ export default {
     // team04-frontend Wiki '디자인 시스템' 문서에서 용도 확인 가능
     colors: {
       white: '#FFFFFF',
-      purple: '#6224FD',
-      purple_hover: '#411F97',
-      purple_disabled: '#C6BBE1',
+      purple: {
+        DEFAULT: '#6224FD',
+        hover: '#411F97',
+        disabled: '#C6BBE1',
+      },
       orange: '#FFA500',
       green: '#8BC48A',
       red: '#FF0101',
       gray: '#EFEFEF',
 
-      text_default: '#232527',
-      text_sub: '#444444',
-      text_disabled: '#A0A0A0',
-      text_success: '#2768FF',
-      text_error: '#FF0101',
+      text: {
+        DEFAULT: '#232527',
+        sub: '#444444',
+        disabled: '#A0A0A0',
+        success: '#2768FF',
+        error: '#FF0101',
+      },
 
       border_defalut: '#BFBFBF',
 
       divider_default: '#E2E2E2',
 
-      Bg_default: '#FFFFFF',
-      Bg_light: '#F5F5F5',
-      Bg_deep: '#EFEFEF',
+      bg: {
+        DEFAULT: '#FFFFFF',
+        light: '#F5F5F5',
+        deep: '#EFEFEF',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
