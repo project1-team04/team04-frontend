8849553b18f821825a845c7b588fa0487c67cdf5
kwonboryong <kwonboryong@naver.com>
2025-01-14 17:07:33 +0900

    style: header 스타일 구조 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
-const HeaderComponent = () => {
-  return <div></div>;
+interface HeaderProps {
+  children: React.ReactNode;
+}
+
+// font-size, font-color, 정렬
+const HeaderComponent = ({ children }: HeaderProps) => {
+  return (
+    <>
+      <div className='border-red-500 h-[100px] border'>
+        <h1 className=''>{children}</h1>
+      </div>
+    </>
+  );
 };
 
 export default HeaderComponent;
