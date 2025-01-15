0800f4c2af78d9d49eb9e648d27d53ea47d88dfc
kwonboryong <kwonboryong@naver.com>
2025-01-15 14:44:24 +0900

    style: 색상값 CSS 변수로 수정 - 폰트 굵기 bold로 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const HeaderComponent = ({ children }: HeaderProps) => {
   return (
     <>
       <div className='flex h-[120px] items-center justify-center'>
-        <h1 className='translate-x-[-240px] transform text-3xl font-semibold text-[#232527]'>
+        <h1 className='text-text-DEFAULT translate-x-[-240px] transform text-3xl font-bold'>
           {children}
         </h1>
       </div>
