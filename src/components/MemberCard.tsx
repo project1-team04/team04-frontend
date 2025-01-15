8111af58130e9dcbdb15d4219ecc61f5fd8fdd13
kwonboryong <kwonboryong@naver.com>
2025-01-15 17:21:32 +0900

    feat: MemberCard 컴포넌트 타입 및 props 추가 - 직책(position), 이름(name), 이메일(email)

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -7,30 +7,33 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 
-// interface MemberCardProps {
-//   position: string,
-//   name: string,
-//   email: string,
-// }
+interface MemberCardProps {
+  position?: 'Project Manager' | 'Member';
+  name: string;
+  email: string;
+}
 
-// props: 직책, 이름, 이메일
 // 직책이 PM과 Member로 나뉨 -> CardTitle의 스타일 달라짐
-const MemberCardComponent = () => {
+const MemberCardComponent = ({
+  position = 'Member',
+  name,
+  email,
+}: MemberCardProps) => {
   return (
     <>
       <Card className='h-[125px] w-[300px] border-border-default'>
         <CardHeader className='m-4 mb-2 flex h-7 w-[130px] justify-center rounded-lg bg-green p-0'>
           <CardTitle className='text-center text-sm font-medium text-white'>
-            Project Manager
+            {position}
           </CardTitle>
         </CardHeader>
 
         <CardContent className='h-8 text-xl font-bold text-text'>
-          <p>정태승</p>
+          <p>{name}</p>
         </CardContent>
 
         <CardFooter className='text-sm text-text-disabled'>
-          <p>hi563@naver.com</p>
+          <p>{email}</p>
         </CardFooter>
       </Card>
     </>
