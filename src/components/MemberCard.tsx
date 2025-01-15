8820b6dba272b5161cb321be407c6ad6ab2b3d04
kwonboryong <kwonboryong@naver.com>
2025-01-15 16:56:31 +0900

    style: 인원 추가 시 멤버 카드 레이아웃 설정 - 카드 레이아웃 설정(width, height, border) - 카드 내부 직책, 이름, 이메일 간격 조정(margin, flex) - 텍스트 폰트 크기, 굵기 및 색상 추가가

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,5 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+// interface MemberCardProps {
+//   position: string,
+//   name: string,
+//   email: string,
+// }
+
+// props: 직책, 이름, 이메일
 const MemberCardComponent = () => {
-  return <div></div>;
+  return (
+    <>
+      <Card className='h-[130px] w-[300px] border-border-default'>
+        <CardHeader className='m-3 flex h-7 w-[130px] justify-center rounded-lg bg-green p-0'>
+          <CardTitle className='text-center text-sm font-medium text-white'>
+            Project Manager
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent className='h-8 text-xl font-bold text-text'>
+          <p>정태승</p>
+        </CardContent>
+
+        <CardFooter className='text-text-disabled'>
+          <p>hi563@naver.com</p>
+        </CardFooter>
+      </Card>
+    </>
+  );
 };
 
 export default MemberCardComponent;
