3c5688a004d618fc1d5026a8091e3e5837777bc7
kwonboryong <kwonboryong@naver.com>
2025-01-15 15:52:59 +0900

    style: 카드 title과 이슈 텍스트 배치치

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,14 +1,18 @@
 import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
 
+// props: 프로젝트명, 이슈 개수
 const ProjectCardComponent = () => {
   return (
     <>
       <Card className='h-[150px] w-[260px] rounded-[15px] border-none'>
-        <CardHeader className='h-[100px] rounded-t-[15px] bg-[#8280FF]/20'>
+        <CardHeader className='flex h-[100px] justify-center rounded-t-[15px] bg-[#8280FF]/20'>
           <CardTitle className='text-2xl'>프로젝트 명</CardTitle>
         </CardHeader>
-        <CardContent>
-          <p className='text-lg'>Issue | 숫자</p>
+
+        <CardContent className='flex justify-end pt-3 text-lg'>
+          <p>
+            Issue | <span className='text-[#00B69B]'>개수</span>
+          </p>
         </CardContent>
       </Card>
     </>
