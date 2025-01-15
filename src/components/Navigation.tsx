5fb16ecfc1b66929822d10135ce41d82f25af862
kwonboryong <kwonboryong@naver.com>
2025-01-15 20:57:34 +0900

    style: 기본 Navigation 컴포넌트 스타일 추가 - nav bar 높이 추가 - 로고 폰트 사이즈, 굵기, 색상 추가 - divider bar 생성 및 스타일 추가

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,16 @@
+const NavigationComponent = () => {
+  return (
+    <>
+      <div>
+        <nav className='flex items-center w-full h-24'>
+          <p className='ml-[10%] text-2xl font-bold text-text'>Threadly</p>
+        </nav>
+        <div className='flex items-center justify-center'>
+          <div className='h-[1px] w-[95%] bg-divider-default'></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavigationComponent;
