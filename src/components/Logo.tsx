157f9c84d67c7dc20e9f071ae722a7948d3ee4c6
kwonboryong <kwonboryong@naver.com>
2025-01-15 23:57:28 +0900

    style: Logo 컴포넌트 스타일 추가 - 로고 텍스트 추가 및 스타일링(폰트 굵기, 사이즈) - 로고 div 비율 배치

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,11 @@
 const Logo = () => {
-  return <div>ddd</div>;
+  return (
+    <>
+      <div className='flex h-[140px] w-2/4 items-end justify-center'>
+        <h1 className='text-5xl font-bold'>Threadly</h1>
+      </div>
+    </>
+  );
 };
 
 export default Logo;
