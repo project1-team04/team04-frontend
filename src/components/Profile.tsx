bc5f06075b43a74262a77217c3c11bf28ba79612
kwonboryong <kwonboryong@naver.com>
2025-01-15 21:48:10 +0900

    style: Profile 컴포넌트 기본 구조 설정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,17 @@
+// import { IoIosArrowDown } from 'react-icons/io';
+
+// props: 이름, 이미지 소스, 이미지 alt
 const ProfileComponent = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className='flex items-center justify-center w-48 h-24 gap-2 bg-gray-hover'>
+        <p className='inline-flex'>권보령</p>
+        <img src='' alt='' className='w-8 h-8 bg-purple' />
+        <p>아이콘</p>
+        {/* <IoIosArrowDown /> */}
+      </div>
+    </>
+  );
 };
 
 export default ProfileComponent;
