679fb78b023012e9cb35d52f7997bfaf293f5822
kwonboryong <kwonboryong@naver.com>
2025-01-15 16:02:38 +0900

    feat: 타입 interface 추가(프로젝트명 title, 이슈 개수 issue)

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,17 +1,21 @@
 import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
 
-// props: 프로젝트명, 이슈 개수
-const ProjectCardComponent = () => {
+interface ProjectCardProps {
+  title: string;
+  issue: number;
+}
+
+const ProjectCardComponent = ({ title, issue }: ProjectCardProps) => {
   return (
     <>
       <Card className='h-[150px] w-[260px] rounded-[15px] border-none'>
         <CardHeader className='flex h-[100px] justify-center rounded-t-[15px] bg-[#8280FF]/20'>
-          <CardTitle className='text-2xl'>프로젝트 명</CardTitle>
+          <CardTitle className='text-2xl'>{title}</CardTitle>
         </CardHeader>
 
         <CardContent className='flex justify-end pt-3 text-lg'>
           <p>
-            Issue | <span className='text-[#00B69B]'>개수</span>
+            Issue | <span className='text-[#00B69B]'>{issue}</span>
           </p>
         </CardContent>
       </Card>
