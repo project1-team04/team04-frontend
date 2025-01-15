1e918adaecaefdc2795329d8766f0a91c5f89976
kwonboryong <kwonboryong@naver.com>
2025-01-15 21:55:18 +0900

    style: arrow 아이콘 추가

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,13 @@
-// import { IoIosArrowDown } from 'react-icons/io';
+import { IoIosArrowDown } from 'react-icons/io';
 
 // props: 이름, 이미지 소스, 이미지 alt
 const ProfileComponent = () => {
   return (
     <>
-      <div className='flex items-center justify-center w-48 h-24 gap-2 bg-gray-hover'>
+      <div className='flex items-center justify-center w-48 h-24 gap-3'>
         <p className='inline-flex'>권보령</p>
-        <img src='' alt='' className='w-8 h-8 bg-purple' />
-        <p>아이콘</p>
-        {/* <IoIosArrowDown /> */}
+        <img src='' alt='' className='rounded-lg h-9 w-9' />
+        <IoIosArrowDown />
       </div>
     </>
   );
