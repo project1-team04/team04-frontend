b01387930857308f2ef3c2c4ea206532d930a7ee
kwonboryong <kwonboryong@naver.com>
2025-01-15 15:13:31 +0900

    style: ProjectCard 컴포넌트 스타일 추가 - 카드 기본 레이아웃 설정 - 카드 border, round, bg 추가

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,18 @@
+import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
+
 const ProjectCardComponent = () => {
-  return <div></div>;
+  return (
+    <>
+      <Card className='h-[150px] w-[260px] rounded-[15px] border-none'>
+        <CardHeader className='h-[100px] rounded-t-[15px] bg-[#8280FF]/20'>
+          <CardTitle className='text-2xl'>프로젝트 명</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className='text-lg'>Issue | 숫자</p>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default ProjectCardComponent;
