9035b65b070bceb1375634f1f3a164ff3771e4cc
kwonboryong <kwonboryong@naver.com>
2025-01-16 11:50:25 +0900

    fix: props에 따른 스타일 변경을 위한 varientStlyes 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,15 +3,25 @@ import { InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type: string;
+  varient?: 'default' | 'createProject';
 }
 
-const InputComponent = ({ type, ...props }: InputProps) => {
+const InputComponent = ({
+  type,
+  varient = 'default',
+  ...props
+}: InputProps) => {
+  const varientStyles = {
+    default: 'w-[250px]',
+    createProject: 'w-[400px]',
+  };
+
   return (
     <>
       <Input
         type={type}
         placeholder={props.placeholder}
-        className={`h-[40px] w-full border-border-default placeholder:text-text-disabled`}
+        className={`h-[43px] ${varientStyles[varient]} m-3 border-border-default placeholder:text-text-disabled`}
         {...props}
       />
     </>
