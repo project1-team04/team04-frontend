f57dd47e3cc5619ab4717c0bc5619be2e14b7094
kwonboryong <kwonboryong@naver.com>
2025-01-16 16:02:35 +0900

    style: z-index 추가 - Material Design 가이드라인 참고

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,8 @@
 const NavigationComponent = () => {
   return (
     <>
-      <div>
-        <nav className='flex items-center w-full h-24'>
+      <div className='z-16'>
+        <nav className='flex h-24 w-full items-center'>
           <p className='ml-[10%] text-2xl font-bold text-text'>Threadly</p>
         </nav>
         <div className='flex items-center justify-center'>
