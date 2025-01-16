64e37af7c0f01745814134f601f4e48b64f2bb80
kwonboryong <kwonboryong@naver.com>
2025-01-16 17:21:43 +0900

    fix: 기존 p 태그 삭제 후 Logo 컴포넌트와 Profile 컴포넌트 추가 - 하위 컴포넌트 배치 및 padding 추가 - Profile 컴포넌트에 필요한 props 추가 및 interface 정의

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,19 @@
-const NavigationComponent = () => {
+import Logo from './Logo';
+import ProfileComponent from './Profile';
+
+interface NavigationProps {
+  name: string;
+  src?: string;
+  alt: string;
+}
+
+const NavigationComponent = ({ name, src, alt }: NavigationProps) => {
   return (
     <>
       <div className='z-16'>
-        <nav className='flex h-24 w-full items-center'>
-          <p className='ml-[10%] text-2xl font-bold text-text'>Threadly</p>
+        <nav className='flex items-center justify-between w-full h-24 px-20'>
+          <Logo />
+          <ProfileComponent name={name} src={src} alt={alt} />
         </nav>
         <div className='flex items-center justify-center'>
           <div className='h-[1px] w-[95%] bg-divider-default'></div>
