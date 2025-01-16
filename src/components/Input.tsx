2a1e48b33b86fe9a2663140ee6fa6d942c880715
kwonboryong <kwonboryong@naver.com>
2025-01-16 15:38:31 +0900

    fix: input은 w-full로 설정, input wrapper에 width 설정 - variantStyles 오타 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,19 +11,21 @@ const InputComponent = ({
   varient = 'default',
   ...props
 }: InputProps) => {
-  const varientStyles = {
+  const variantStyles = {
     default: 'w-[250px]',
     createProject: 'w-[400px]',
   };
 
   return (
     <>
-      <Input
-        type={type}
-        placeholder={props.placeholder}
-        className={`h-[43px] ${varientStyles[varient]} m-3 border-border-default placeholder:text-text-disabled`}
-        {...props}
-      />
+      <div className={`${variantStyles[varient]}`}>
+        <Input
+          type={type}
+          placeholder={props.placeholder}
+          className='h-[43px] w-full border-border-default placeholder:text-text-disabled'
+          {...props}
+        />
+      </div>
     </>
   );
 };
