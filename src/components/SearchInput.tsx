3e54b9c1a775778b162f9614eb0ae30ac2a41fb2
kwonboryong <kwonboryong@naver.com>
2025-01-16 11:31:04 +0900

    style: 아이콘 추가 및 스타일 상세 조정 - search 아이콘 추가 - 전체 input container 수정(크기, 배치, 정렬, 색상) - input 그림자, border 수정 - 포커싱 시 표시되는 효과 제거거

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,21 @@
 import { Input } from './ui/input';
 import { InputHTMLAttributes } from 'react';
+import { IoSearchOutline } from 'react-icons/io5';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const SearchInput = ({ ...props }: InputProps) => {
   return (
     <>
-      <Input
-        type='text'
-        placeholder='Search for anything...'
-        className={`h-[43px] w-[180px] border-none bg-gray text-sm placeholder:text-text-disabled`}
-        {...props}
-      />
+      <div className='flex h-[43px] w-[200px] items-center justify-center rounded-lg border-none bg-gray'>
+        <IoSearchOutline className='ml-3' />
+        <Input
+          type='text'
+          placeholder='Search for anything...'
+          className={`h-[43px] w-[170px] border-0 border-none bg-gray text-sm shadow-none ring-0 placeholder:text-text-disabled focus-visible:ring-0 focus-visible:ring-offset-0`}
+          {...props}
+        />
+      </div>
     </>
   );
 };
