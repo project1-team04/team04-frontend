3b4244d049f1f1b88e3c1f6fc3e0dd6e42eed2b7
kwonboryong <kwonboryong@naver.com>
2025-01-17 15:53:28 +0900

    feat: 조건부로 페이지에 따라 레이아웃 변경 - 서치바는는 이슈 상세 페이지에서만 표시 - 채팅은 이슈 상세 페이지에서만 표시

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import HeaderComponent from '@/components/Header';
 import NavigationComponent from '@/components/Navigation';
 import { ReactNode } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useMatch } from 'react-router-dom';
 
 interface MainLayoutProps {
   children?: ReactNode;
@@ -18,22 +18,40 @@ const MainLayout = ({ children }: MainLayoutProps) => {
 
   const headerTitle = path[location.pathname];
 
+  // 특정 경로 매칭: /projects/:projectId
+  // const isProjectDetailPage = useMatch('/projects/:projectId');
+
+  // 임시로 /profile, /profile/delegate 페이지로 설정
+  const isProjectDetailPage = useMatch('/profile');
+  const isProjectsIssueDetailPage = useMatch('/profile/delegate');
+
   return (
     <>
       <div className='flex h-[100vh] w-full flex-col'>
         <NavigationComponent name={'권보령'} alt={'프로필 사진'} />
 
         <div className='flex flex-grow items-center justify-center'>
-          <div className='h-full w-[700px]'>
-            <header className='ml-14 flex h-[120px] items-center'>
+          {isProjectDetailPage && (
+            <div className='h-[100%] w-[400px] bg-purple'>서치바</div>
+          )}
+
+          <div
+            className={`flex h-full ${
+              isProjectDetailPage || isProjectsIssueDetailPage
+                ? 'w-full'
+                : 'w-[800px]'
+            } flex-col bg-orange`}
+          >
+            <header className='ml-14 flex h-[110px] items-center'>
               <HeaderComponent children={headerTitle} />
             </header>
-            <main>
-              {/* <div>서치바</div> */}
-              {children || <Outlet />}
-              {/* <div>채팅</div> */}
-            </main>
+
+            <main className='flex-grow bg-red'>{children || <Outlet />}</main>
           </div>
+
+          {isProjectsIssueDetailPage && (
+            <div className='h-[100%] w-[600px] bg-purple'>채팅</div>
+          )}
         </div>
       </div>
     </>
