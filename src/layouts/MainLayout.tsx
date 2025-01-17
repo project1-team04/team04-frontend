34cae15f944098ef160f996c81ff51dbf5dcbf0d
Hyerim Yang <hlhlstar@naver.com>
2025-01-17 15:31:55 +0900

    chore: 완료된 TODO 주석 제거

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,9 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
-    // Layout 및 Outlet 렌더링 확인을 위해 임시 스타일 적용
-    // TODO: 라우팅 작업 완료시 스타일 제거
-    <div className='h-[100vh] w-full bg-green'>
+    <div>
       <Outlet />
     </div>
   );
