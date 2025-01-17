aadb214bfa0f01caa932df3385318fe0aede66e8
kwonboryong <kwonboryong@naver.com>
2025-01-17 13:56:10 +0900

    feat: Logo 컴포넌트 추가 및 기본 스타일 추가 - 레이아웃 분할을 위한 div 태그 추가 및 스타일 적용 - Logo 컴포넌트 추가

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,23 @@
+import Logo from '@/components/Logo';
 import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
   return (
-    // Layout 및 Outlet 렌더링 확인을 위해 임시 스타일 적용
-    // TODO: 라우팅 작업 완료시 스타일 제거
-    <div className='bg-orange h-[100vh] w-full'>
-      <Outlet />
-    </div>
+    <>
+      <div className='flex h-[100vh] w-full'>
+        <div className='w-1/2 bg-purple'>왼쪽 부분</div>
+
+        <div className='flex w-1/2 flex-col'>
+          <div className='flex h-[25%] items-end justify-center'>
+            <Logo variant='loginLogo' />
+          </div>
+
+          <div className='h-4/5'>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
