519cf8bc926d7c688ff4908e4c453bd9905d1fca
kwonboryong <kwonboryong@naver.com>
2025-01-17 13:25:52 +0900

    feat: 공통 컴포넌트 조립 및 경로 지정 - Header, Navigation 컴포넌트 조립 - 경로에 따른 Header 컴포넌트의 title 변경을 위해 location 추가

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,31 @@
+import HeaderComponent from '@/components/Header';
+import NavigationComponent from '@/components/Navigation';
 import { ReactNode } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 interface MainLayoutProps {
   children?: ReactNode;
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const location = useLocation();
+
+  const headerPath: Record<string, string> = {
+    '/projects': '내 프로젝트',
+    '/projects/new': '프로젝트 생성',
+    '/profile/delegate': '프로젝트 권한 위임',
+  };
+
+  const headerTitle = headerPath[location.pathname];
+
   return (
-    // Layout 및 Outlet 렌더링 확인을 위해 임시 스타일 적용
-    // TODO: 라우팅 작업 완료시 스타일 제거
-    <div className='bg-green h-[100vh] w-full'>{children || <Outlet />}</div>
+    <>
+      <div>
+        <NavigationComponent name={'권보령'} alt={'프로필 사진'} />
+        <HeaderComponent children={headerTitle} />
+        <div className='w-full'>{children || <Outlet />}</div>
+      </div>
+    </>
   );
 };
 
