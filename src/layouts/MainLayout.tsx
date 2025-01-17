9c89b7ee2598a784dbe58b0bbc9f7bd507a5ee82
kwonboryong <kwonboryong@naver.com>
2025-01-17 15:19:44 +0900

    style: Header 컴포넌트 위치 적용

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -10,20 +10,31 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
   const location = useLocation();
 
-  const headerPath: Record<string, string> = {
+  const path: Record<string, string> = {
     '/projects': '내 프로젝트',
     '/projects/new': '프로젝트 생성',
     '/profile/delegate': '프로젝트 권한 위임',
   };
 
-  const headerTitle = headerPath[location.pathname];
+  const headerTitle = path[location.pathname];
 
   return (
     <>
-      <div className='h-[100vh] w-full'>
+      <div className='flex h-[100vh] w-full flex-col'>
         <NavigationComponent name={'권보령'} alt={'프로필 사진'} />
-        <HeaderComponent children={headerTitle} />
-        {children || <Outlet />}
+
+        <div className='flex flex-grow items-center justify-center'>
+          <div className='h-full w-[700px]'>
+            <header className='ml-14 flex h-[120px] items-center'>
+              <HeaderComponent children={headerTitle} />
+            </header>
+            <main>
+              {/* <div>서치바</div> */}
+              {children || <Outlet />}
+              {/* <div>채팅</div> */}
+            </main>
+          </div>
+        </div>
       </div>
     </>
   );
