caa75b6d6a8eb4ce56e747244d3657bc10901dfd
kwonboryong <kwonboryong@naver.com>
2025-01-18 20:02:14 +0900

    fix: 카드 UI 배치를 위한 사이즈 조정 -  카드 height, width - 카드 헤더 height - 이슈 폰트 사이즈

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,14 +10,14 @@ const ProjectCardComponent = ({ title, issue, onClick }: ProjectCardProps) => {
   return (
     <>
       <Card
-        className='h-[150px] w-[260px] rounded-[15px] border-none'
+        className='h-[130px] w-[230px] rounded-[15px] border-none'
         onClick={onClick}
       >
-        <CardHeader className='flex h-[100px] justify-center rounded-t-[15px] bg-[#8280FF]/20'>
+        <CardHeader className='flex h-[85px] justify-center rounded-t-[15px] bg-[#8280FF]/20'>
           <CardTitle className='text-2xl'>{title}</CardTitle>
         </CardHeader>
 
-        <CardContent className='flex justify-end pt-3 text-lg'>
+        <CardContent className='flex justify-end pt-3 text-base'>
           <p>
             Issue | <span className='text-[#00B69B]'>{issue}</span>
           </p>
