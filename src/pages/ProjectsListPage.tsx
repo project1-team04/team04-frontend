504480bc3f28e34d1f0e96b3363fbe18ff81e2b7
kwonboryong <kwonboryong@naver.com>
2025-01-18 20:06:36 +0900

    style: Grid 3x3 배치

--- src/pages/ProjectsListPage.tsx
+++ src/pages/ProjectsListPage.tsx
@@ -5,11 +5,17 @@ const ProjectsListPage = () => {
     { id: '1', title: 'Threadly', issue: 1 },
     { id: '2', title: 'Day6', issue: 2 },
     { id: '3', title: 'Bee', issue: 3 },
+    { id: '4', title: 'Project X', issue: 4 },
+    { id: '5', title: 'Alpha', issue: 5 },
+    { id: '6', title: 'Beta', issue: 6 },
+    { id: '7', title: 'Gamma', issue: 7 },
+    { id: '8', title: 'Delta', issue: 8 },
+    { id: '9', title: 'Epsilon', issue: 9 },
   ];
 
   return (
     <>
-      <div className='grid grid-cols-3 gap-4 p-4'>
+      <div className='grid grid-cols-3 gap-5'>
         {data.map((project) => (
           <ProjectCardComponent
             key={project.id}
