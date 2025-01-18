3a89ac254357c401e057f14a5ddfc06d31522922
Hyerim Yang <hlhlstar@naver.com>
2025-01-18 16:41:50 +0900

    fix: ButtonComponent variantStyles 미적용 문제 해결
    - Tailwind 커스텀 컬러를 `extend` 내부로 옮긴 후, ButtonComponent에서 variantStyles가 적용되지 않는 문제 발생
    - buttonVariants에 ButtonComponent의 variantStyles 스타일을 반영하여 해결
    - ButtonComponent에서 variant와 size를 Button에 직접 전달하도록 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,23 +4,20 @@ import { ButtonHTMLAttributes } from 'react';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   variant?: 'primary' | 'secondary' | 'outline' | 'negative';
+  size?: 'default' | 'sm' | 'lg' | 'icon';
 }
 
 const ButtonComponent = ({
   children,
   variant = 'primary',
+  size = 'default',
   ...props
 }: ButtonProps) => {
-  const variantStyles = {
-    primary: 'bg-purple hover:bg-purple-hover text-white',
-    secondary: 'bg-gray hover:bg-gray-hover',
-    outline: 'border border-default hover:bg-gray-hover',
-    negative: 'bg-red hover:bg-red-hover text-white',
-  };
-
   return (
     <Button
-      className={`${variantStyles[variant]} w-full text-center text-base`}
+      variant={variant}
+      size={size}
+      className={`w-full text-center text-base`}
       {...props}
     >
       {children}
