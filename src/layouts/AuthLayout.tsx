12270a2c08935b918ec2fd4475fdf5b33170d20c
Hyerim Yang <hlhlstar@naver.com>
2025-01-18 17:38:43 +0900

    feat: 비밀번호 찾기 페이지 UI 퍼블리싱

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -10,9 +10,6 @@ const AuthLayout = () => {
         <div className='flex w-1/2 flex-col items-center justify-center'>
           <header className='flex flex-col items-center gap-4'>
             <Logo variant='authLogo' />
-            <p className='text-sm text-text-sub'>
-              팀원들과 실시간으로 프로젝트를 관리해보세요.
-            </p>
           </header>
 
           <main>
--- src/pages/AuthFindPasswordPage.tsx
+++ src/pages/AuthFindPasswordPage.tsx
@@ -1,5 +1,27 @@
+import { Separator } from '@/components/ui/separator';
+import ButtonComponent from '@/components/Button';
+import InputComponent from '@/components/Input';
+
 const AuthForgotPasswordPage = () => {
-  return <div>AuthForgotPasswordPage</div>;
+  return (
+    <div className='mt-6 flex flex-col items-center gap-y-6'>
+      {/* TODO) 폼 제출 후 바로 로그인 페이지로 리다이렉트, 임시 비밀번호로 로그인하면 바로 마이페이지로 리다이렉트 */}
+      {/* TODO) 이메일이 등록된 계정인지 확인 후 폼 제출할 수 있게 할 것인지 논의 */}
+      <p className='text-sm text-text-sub'>
+        가입하신 이메일로 임시 비밀번호를 보내드릴게요.
+      </p>
+      <form className='flex w-[250px] flex-col gap-2'>
+        <InputComponent type='email' placeholder='이메일' />
+        <ButtonComponent>임시 비밀번호 전송</ButtonComponent>
+      </form>
+
+      <div className='flex h-4 items-center gap-4 text-xs text-text-disabled'>
+        <p className='hover:text-text-sub'>로그인하기</p>
+        <Separator orientation='vertical' />
+        <p className='hover:text-text-sub'>비밀번호찾기</p>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForgotPasswordPage;
