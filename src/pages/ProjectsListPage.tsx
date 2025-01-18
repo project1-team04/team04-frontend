03239941c1d81f1560125026e845a5d20c09e7d6
kwonboryong <kwonboryong@naver.com>
2025-01-18 19:38:55 +0900

    style: ProjectCard 컴포넌트 추가 및 목 데이터 생성

--- src/pages/ProjectsListPage.tsx
+++ src/pages/ProjectsListPage.tsx
@@ -1,5 +1,23 @@
+import ProjectCardComponent from '@/components/ProjectCard';
+
 const ProjectsListPage = () => {
-  return <div>내 프로젝트</div>;
+  const data = [
+    { id: '1', title: 'Threadly', issue: 1 },
+    { id: '2', title: 'Day6', issue: 2 },
+    { id: '3', title: 'Bee', issue: 3 },
+  ];
+
+  return (
+    <>
+      <ProjectCardComponent
+        title={''}
+        issue={0}
+        onClick={function (): void {
+          throw new Error('Function not implemented.');
+        }}
+      />
+    </>
+  );
 };
 
 export default ProjectsListPage;
