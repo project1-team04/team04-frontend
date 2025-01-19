e779a1cf72f23b18ebaed36c74ed1f0027aceb4b
kwonboryong <kwonboryong@naver.com>
2025-01-19 23:47:54 +0900

    style: 검색 결과 스타일 추가

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,8 +2,14 @@ import SearchInput from './SearchInput';
 
 const Search = () => {
   return (
-    <div>
+    <div className='flex h-full flex-col items-center p-10'>
       <SearchInput />
+
+      <div className='mt-3 divide-y divide-divider-default'>
+        <p className='flex flex-col items-center p-1 text-sm'>
+          이슈명1 <span className='p-1 text-xs'>담당자</span>
+        </p>
+      </div>
     </div>
   );
 };
