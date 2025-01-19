d1826130795c302d6fa34f9d9ae06bcdaf6257cd
kwonboryong <kwonboryong@naver.com>
2025-01-19 10:09:41 +0900

    fix: projectsLayout 사용

--- src/pages/ProjectsCreatePage.tsx
+++ src/pages/ProjectsCreatePage.tsx
@@ -1,6 +1,4 @@
-import ButtonComponent from '@/components/Button';
-import InputComponent from '@/components/Input';
-import MemberCardComponent from '@/components/MemberCard';
+import ProjectsLayout from '@/components/ProjectsLayout';
 
 interface Member {
   id: string;
@@ -26,41 +24,7 @@ const data: Member[] = [
 const ProjectsCreatePage = () => {
   return (
     <>
-      <div className='ml-3 flex items-center gap-5'>
-        <p>프로젝트 이름</p>
-        <div className='w-[86%]'>
-          <InputComponent placeholder="Enter Project's name" />
-        </div>
-      </div>
-
-      <div className='mt-4 h-[315px] overflow-auto bg-gray p-3'>
-        <div className='align-items-center m-5 mt-3 grid grid-cols-2 justify-items-center gap-y-4'>
-          {data.map((member) => (
-            <MemberCardComponent
-              key={member.id}
-              name={member.name}
-              email={member.email}
-              position={member.position}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div className='mt-5 flex flex-col gap-4'>
-        <ButtonComponent
-          variant='secondary'
-          children={'+ 인원 추가'}
-          onClick={() => {
-            console.log('인원 추가 버튼 클릭');
-          }}
-        />
-        <ButtonComponent
-          children={'생성 완료'}
-          onClick={() => {
-            console.log('생성 완료 버튼 클릭');
-          }}
-        />
-      </div>
+      <ProjectsLayout data={data} />
     </>
   );
 };
