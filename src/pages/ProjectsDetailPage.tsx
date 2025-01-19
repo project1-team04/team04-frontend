429972c978e9e2d52611913115f1f0ff2d63ee7f
kwonboryong <kwonboryong@naver.com>
2025-01-19 14:39:30 +0900

    style: 프로젝트 디테일 페이지 기본 레이아웃 설정

--- src/pages/ProjectsDetailPage.tsx
+++ src/pages/ProjectsDetailPage.tsx
@@ -1,5 +1,22 @@
 const ProjectsDetailPage = () => {
-  return <div>Project Name</div>;
+  return (
+    <>
+      <div className='flex h-full gap-6'>
+        <div className='h-full w-[330px] rounded-t-2xl bg-bg-deep p-5'>
+          {/* divide-y divide-gray-300 */}
+          <div className='mb-3 flex h-[45px] gap-4 bg-orange'>
+            <div>상태 원</div>
+            <span>To do(status)</span>
+            <span>이슈 개수(숫자)</span>
+            {/* divider 역으로 border bottom? 넣기 */}
+          </div>
+          <div className='h-[130px] w-full rounded-lg bg-red'>
+            이슈 카드 컴포넌트
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectsDetailPage;
