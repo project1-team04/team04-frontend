ba74d9773efca265f93b1d25c06d376404ecc1bc
kwonboryong <kwonboryong@naver.com>
2025-01-19 09:52:42 +0900

    feat: 데이터 패칭을 위한 interface 추가

--- src/pages/ProjectsCreatePage.tsx
+++ src/pages/ProjectsCreatePage.tsx
@@ -2,21 +2,28 @@ import ButtonComponent from '@/components/Button';
 import InputComponent from '@/components/Input';
 import MemberCardComponent from '@/components/MemberCard';
 
-const ProjectsCreatePage = () => {
-  // 목 데이터
-  const data = [
-    {
-      id: '1',
-      name: '정태승',
-      email: 'hfgdf3@naver.com',
-      position: 'Project Manager',
-    },
-    { id: '2', name: '권보령', email: 'hfgdf3@naver.com' },
-    { id: '3', name: '양혜림', email: 'hfgdf3@naver.com' },
-    { id: '4', name: '이태정', email: 'hfgdf3@naver.com' },
-    // { id: '5', name: '명광호', email: 'hfgdf3@naver.com', position: 'Member' },
-  ];
+interface Member {
+  id: string;
+  name: string;
+  email: string;
+  position?: 'Project Manager' | 'Member';
+}
 
+// 목 데이터
+const data: Member[] = [
+  {
+    id: '1',
+    name: '정태승',
+    email: 'hfgdf3@naver.com',
+    position: 'Project Manager',
+  },
+  { id: '2', name: '권보령', email: 'hfgdf3@naver.com' },
+  { id: '3', name: '양혜림', email: 'hfgdf3@naver.com' },
+  { id: '4', name: '이태정', email: 'hfgdf3@naver.com' },
+  // { id: '5', name: '명광호', email: 'hfgdf3@naver.com', position: 'Member' },
+];
+
+const ProjectsCreatePage = () => {
   return (
     <>
       <div className='ml-3 flex items-center gap-5'>
