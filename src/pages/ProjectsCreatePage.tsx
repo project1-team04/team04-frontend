3af25ef50639ace7abb404b47393a1d4f05c2dc3
kwonboryong <kwonboryong@naver.com>
2025-01-19 09:42:01 +0900

    feat: 목 데이터 추가와 카드 동적 생성

--- src/pages/ProjectsCreatePage.tsx
+++ src/pages/ProjectsCreatePage.tsx
@@ -1,5 +1,25 @@
+import MemberCardComponent from '@/components/MemberCard';
+
 const ProjectsCreatePage = () => {
-  return <div>프로젝트 생성</div>;
+  // 목 데이터
+  const data = [
+    {
+      id: '1',
+      name: '정태승',
+      email: 'hfgdf3@naver.com',
+      position: 'Project Manager',
+    },
+    { id: '2', name: '권보령', email: 'hfgdf3@naver.com' },
+    { id: '3', name: '양혜림', email: 'hfgdf3@naver.com' },
+    { id: '4', name: '이태정', email: 'hfgdf3@naver.com' },
+    // { id: '5', name: '명광호', email: 'hfgdf3@naver.com', position: 'Member' },
+  ];
+
+  return (
+    <>
+      <MemberCardComponent name={''} email={''} />
+    </>
+  );
 };
 
 export default ProjectsCreatePage;
