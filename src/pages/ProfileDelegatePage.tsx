62c2eb76e75b6f1e41949320dbce4118eb0b8674
kwonboryong <kwonboryong@naver.com>
2025-01-19 13:39:33 +0900

    feat: 목 데이터에 따른 프로젝트 권한 위임 컴포넌트 동적으로 생성

--- src/pages/ProfileDelegatePage.tsx
+++ src/pages/ProfileDelegatePage.tsx
@@ -1,13 +1,29 @@
 import Button from '@/components/Button';
 import ProjectDelegation from '@/components/ProjectDelegation';
 
+// 목 데이터터
+const data = [
+  { id: '1', title: 'Threadly', member: ['앵무새새', '고양이', '강아지'] },
+  { id: '2', title: 'Apple', member: ['권', '보보', '령령'] },
+  { id: '3', title: 'Banana', member: ['ㅇㅇ', 'ㄴㄴ', 'ㅈ'] },
+  // { id: '3', title: 'Banna', member: ['ㅇㅇ', 'ㄴㄴ', 'ㅈ'] },
+  // { id: '3', title: 'Banna', member: ['ㅇㅇ', 'ㄴㄴ', 'ㅈ'] },
+  // { id: '3', title: 'Banna', member: ['ㅇㅇ', 'ㄴㄴ', 'ㅈ'] },
+];
+
 const ProfileDelegatePage = () => {
   return (
     <>
       <div className='m-auto flex h-[90%] w-[75%] flex-col items-center rounded-2xl bg-gray p-8'>
         {/* 구간 스크롤 */}
-        <div className='flex h-[87%] w-4/5 flex-col bg-orange'>
-          <ProjectDelegation />
+        <div className='flex h-[87%] w-4/5 flex-col'>
+          {data.map((project) => (
+            <ProjectDelegation
+              key={project.id}
+              title={project.title}
+              member={project.member}
+            />
+          ))}
         </div>
 
         <div className='mt-5 flex w-3/5 gap-5'>
