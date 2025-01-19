ebf9a7330541468fb306b9c4f7f6f679a9c26a7c
kwonboryong <kwonboryong@naver.com>
2025-01-19 20:20:18 +0900

    style: 이슈 카드 컴포넌트 스타일링 추가

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -1,8 +1,13 @@
+// props: 이슈 타이틀, 이슈 아이디, 담당자 명
 const IssueCard = () => {
   return (
     <>
-      <div className='h-[130px] w-full rounded-lg bg-red'>
-        이슈 카드 컴포넌트
+      <div className='h-[130px] w-full rounded-lg bg-bg p-7'>
+        <span className='text-lg font-bold'>이슈 타이틀</span>
+        <div className='flex justify-between mt-4 text-sm'>
+          <span>이슈 아이디</span>
+          <span>담당자 명</span>
+        </div>
       </div>
     </>
   );
