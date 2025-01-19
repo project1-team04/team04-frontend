64a75064f6fe39ac3246b0c9e4e422da4b4e1b50
kwonboryong <kwonboryong@naver.com>
2025-01-19 13:25:31 +0900

    style: 프로젝트 권한 위임 컴포넌트 스타일링

--- src/components/ProjectDelegation.tsx
+++ src/components/ProjectDelegation.tsx
@@ -1,10 +1,11 @@
 import DropDown from './DropDown';
 
+// 프로젝트 명, 해당 프로젝트의 멤버들
 const ProjectDelegation = () => {
   return (
     <div>
-      <div className='flex h-[100px] w-full gap-10 rounded-xl bg-red p-4'>
-        <div className='text-lg font-semibold text-text'>프로젝트명</div>
+      <div className='flex h-[60px] w-full items-center justify-between gap-10 rounded-xl bg-white p-8 pl-10'>
+        <span className='text-lg font-semibold text-text'>Threadly</span>
 
         <DropDown buttonText={'위임'} items={['멤버1', '멤버2']} />
       </div>
