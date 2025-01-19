74808fbff775ffde47d7ccc5d7b9e02b0b748fab
kwonboryong <kwonboryong@naver.com>
2025-01-19 23:22:03 +0900

    feat: 스타일 추가 및 props 추가 - 상태에 따른 스타일 변경을 위해 변수 설정

--- src/components/KanbanCard.tsx
+++ src/components/KanbanCard.tsx
@@ -1,19 +1,43 @@
 import IssueCard from './IssueCard';
 
-// props: status
-const KanbanCard = () => {
+interface KanbanCardProps {
+  status: 'To Do' | 'On Progress' | 'Done';
+  issueCount: string;
+}
+
+// props: status, issueCount, data
+const KanbanCard = ({ status, issueCount }: KanbanCardProps) => {
+  const variantStyles = {
+    'To Do': {
+      divideColor: 'divide-purple',
+      roundColor: 'bg-purple',
+    },
+    'On Progress': {
+      divideColor: 'divide-orange',
+      roundColor: 'bg-orange',
+    },
+    Done: {
+      divideColor: 'divide-green',
+      roundColor: 'bg-green',
+    },
+  };
+
+  const { divideColor, roundColor } = variantStyles[status];
+
   return (
     <>
-      <div className='h-full w-[330px] rounded-t-2xl bg-bg-deep p-5'>
-        {/* divide-y divide-gray-300 */}
-        <div className='mb-3 flex h-[45px] gap-4 bg-orange'>
-          <div>상태 원</div>
-          <span>To do(status)</span>
-          <span>이슈 개수(숫자)</span>
-          {/* divider 역으로 border bottom? 넣기 */}
+      <div
+        className={`h-full w-[330px] divide-y-2 ${divideColor} rounded-t-2xl bg-bg-deep p-5`}
+      >
+        <div className='mb-3 flex h-[35px] items-center gap-4'>
+          <div className={`h-3 w-3 rounded-full ${roundColor}`} />
+          <span className='font-semibold'>{status}</span>
+          <span className='h-5 w-5 rounded-lg bg-border-default text-center text-sm'>
+            {issueCount}
+          </span>
         </div>
 
-        <section className='flex flex-col gap-5'>
+        <section className='flex flex-col gap-5 pt-4'>
           <IssueCard
             title={'Merge 이슈'}
             issueId={'12345'}
