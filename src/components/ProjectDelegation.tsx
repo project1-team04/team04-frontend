b44c787473ff1e4bd304c60eec7dedb419cec142
kwonboryong <kwonboryong@naver.com>
2025-01-19 13:39:04 +0900

    feat: 프로젝트 권한 위임 페이지에서 프로젝트명과 멤버 데이터를 props로 전달 받을 수 있도록 추가

--- src/components/ProjectDelegation.tsx
+++ src/components/ProjectDelegation.tsx
@@ -1,15 +1,19 @@
 import DropDown from './DropDown';
 
-// 프로젝트 명, 해당 프로젝트의 멤버들
-const ProjectDelegation = () => {
-  return (
-    <div>
-      <div className='flex h-[60px] w-full items-center justify-between gap-10 rounded-xl bg-white p-8 pl-10'>
-        <span className='text-lg font-semibold text-text'>Threadly</span>
+interface ProjectDelegationProps {
+  title: string;
+  member: string[];
+}
 
-        <DropDown buttonText={'위임'} items={['멤버1', '멤버2']} />
+const ProjectDelegation = ({ title, member }: ProjectDelegationProps) => {
+  return (
+    <>
+      <div className='my-2 flex h-[60px] w-full items-center justify-between gap-10 rounded-xl bg-white p-8 pl-10'>
+        <span className='text-lg font-semibold text-text'>{title}</span>
+
+        <DropDown buttonText={'위임'} items={member} />
       </div>
-    </div>
+    </>
   );
 };
 
