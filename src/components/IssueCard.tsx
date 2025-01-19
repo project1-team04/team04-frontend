bcc031aa1dbb661ca94e8e6d154775464aa12953
kwonboryong <kwonboryong@naver.com>
2025-01-19 20:13:16 +0900

    chore: 이슈 카드 컴포넌트 생성

---
+++ src/components/IssueCard.tsx
@@ -0,0 +1,11 @@
+const IssueCard = () => {
+  return (
+    <>
+      <div className='h-[130px] w-full rounded-lg bg-red'>
+        이슈 카드 컴포넌트
+      </div>
+    </>
+  );
+};
+
+export default IssueCard;
