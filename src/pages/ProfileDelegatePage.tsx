4ded4bc06d4ba8f003458b77edbf0b2a298bf3a5
kwonboryong <kwonboryong@naver.com>
2025-01-19 11:47:59 +0900

    fix: 프로젝트 권한 위임 컴포넌트 추가

--- src/pages/ProfileDelegatePage.tsx
+++ src/pages/ProfileDelegatePage.tsx
@@ -1,18 +1,13 @@
 import Button from '@/components/Button';
-import DropDown from '@/components/DropDown';
+import ProjectDelegation from '@/components/ProjectDelegation';
 
 const ProfileDelegatePage = () => {
   return (
     <>
       <div className='m-auto flex h-[90%] w-[75%] flex-col items-center rounded-2xl bg-gray p-8'>
+        {/* 구간 스크롤 */}
         <div className='flex h-[87%] w-4/5 flex-col bg-orange'>
-          {/* 프로젝트 목록 보여주기 => 컴포넌트화 */}
-          {/* 구간 스크롤 */}
-          <div className='flex h-[100px] w-full gap-10 rounded-xl bg-red p-4'>
-            <div className='text-lg font-semibold text-text'>프로젝트명</div>
-
-            <DropDown buttonText={'위임'} items={['멤버1', '멤버2']} />
-          </div>
+          <ProjectDelegation />
         </div>
 
         <div className='mt-5 flex w-3/5 gap-5'>
