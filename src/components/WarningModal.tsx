d77fbb16c323ba7134e06e0a96458a7ebc8715ee
kwonboryong <kwonboryong@naver.com>
2025-01-20 15:09:24 +0900

    style: 버튼과 경고 아이콘 추가

--- src/components/WarningModal.tsx
+++ src/components/WarningModal.tsx
@@ -1,5 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { RiErrorWarningLine } from 'react-icons/ri';
 const WarningModal = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className='flex h-[300px] w-[400px] flex-col items-center justify-center bg-bg-light'>
+        <RiErrorWarningLine className='h-[60px] w-[60px]' />
+        <div className='m-6 mt-7 text-center'>
+          <p className='text-2xl font-semibold'>정말 탈퇴 하시겠습니까?</p>
+          <p className='font-base text-sm'>
+            탈퇴 후에는 해당 이메일로 재가입이 불가능합니다.
+          </p>
+        </div>
+        <div className='flex'>
+          <Button variant={'outline'} children={'아니오'} />
+          <Button variant={'negative'} children={'네'} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WarningModal;
