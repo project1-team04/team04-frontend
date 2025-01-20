f5d62dcdcd11b25fe751057c7af9f9ca926ecf6c
Hyerim Yang <hlhlstar@naver.com>
2025-01-20 16:48:20 +0900

    refactor: ProjectDelegation 내 DropDown을 Selector로 변경

--- src/components/ProjectDelegation.tsx
+++ src/components/ProjectDelegation.tsx
@@ -1,4 +1,10 @@
-import DropDown from './DropDown';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 interface ProjectDelegationProps {
   title: string;
@@ -11,7 +17,20 @@ const ProjectDelegation = ({ title, member }: ProjectDelegationProps) => {
       <div className='my-2 flex h-[60px] w-full items-center justify-between gap-10 rounded-xl bg-white p-8 pl-10'>
         <span className='text-lg font-semibold text-text'>{title}</span>
 
-        <DropDown buttonText={'위임'} items={member} />
+        <Select>
+          <SelectTrigger className='w-fit'>
+            <SelectValue placeholder='위임' />
+          </SelectTrigger>
+          <SelectContent className='min-w-fit'>
+            {member.map((memberName) => (
+              // FIX) 멤버 고유 id로 key 변경
+              // 동명이인이 있을 수 있으므로 memberName은 key로 부적합
+              <SelectItem key={memberName} value={memberName}>
+                {memberName}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </div>
     </>
   );
