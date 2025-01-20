b8d4f197224b8489b01a8e3d45afdbeb7873ad1b
kwonboryong <kwonboryong@naver.com>
2025-01-20 17:25:23 +0900

    feat: 경고 모달창 props 추가

--- src/components/WarningModal.tsx
+++ src/components/WarningModal.tsx
@@ -3,10 +3,15 @@ import { RiErrorWarningLine } from 'react-icons/ri';
 import Backdrop from './Backdrop';
 import { useModalStore } from '@/stores/useModalStore';
 
-const WarningModal = () => {
+interface WarningModalProps {
+  title: string;
+  sub: string;
+}
+
+const WarningModal = ({ title, sub }: WarningModalProps) => {
   const { isOpen, close } = useModalStore();
 
-  if (!isOpen) return null; // 모달이 열려 있지 않으면 아무것도 렌더링하지 않음
+  if (!isOpen) return null;
 
   return (
     <>
@@ -15,10 +20,8 @@ const WarningModal = () => {
         <div className='flex h-[300px] w-[400px] flex-col items-center justify-center bg-bg-light'>
           <RiErrorWarningLine className='h-[60px] w-[60px]' />
           <div className='m-6 mt-7 text-center'>
-            <p className='text-2xl font-semibold'>정말 탈퇴 하시겠습니까?</p>
-            <p className='font-base text-sm'>
-              탈퇴 후에는 해당 이메일로 재가입이 불가능합니다.
-            </p>
+            <p className='text-2xl font-semibold'>{title}</p>
+            <p className='font-base text-sm'>{sub}</p>
           </div>
           <div className='flex gap-x-3'>
             <Button variant={'outline'} children={'아니오'} onClick={close} />
