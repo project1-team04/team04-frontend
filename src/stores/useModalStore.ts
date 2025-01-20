84534326062fc84e9544de833e2ee07e7a926490
kwonboryong <kwonboryong@naver.com>
2025-01-20 16:24:49 +0900

    chore: 모달 전역 관리를 위한 스토어 생성

---
+++ src/stores/useModalStore.ts
@@ -0,0 +1,14 @@
+// import { create } from 'zustand';
+
+// interface ModalStore {
+//   content: React.ReactNode | null;
+//   isOpen: boolean;
+//   open: (content: React.ReactNode) => void;
+//   close: () => void;
+// }
+// export const useModalStore = create<ModalStore>((set) => ({
+//   content: null,
+//   isOpen: false,
+//   open: (content) => set({ isOpen: true, content }),
+//   close: () => set({ isOpen: false }),
+// }));
