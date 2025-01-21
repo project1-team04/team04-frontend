2fb002fbde39dcb744c6fa1c860fd1d60d312cee
kwonboryong <kwonboryong@naver.com>
2025-01-21 21:31:17 +0900

    fix: 시맨틱 태그 추가가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ interface HeaderProps {
 
 const Header = ({ children }: HeaderProps) => {
   return (
-    <>
+    <header>
       <h1 className='inline-flex text-3xl font-bold'>{children}</h1>
-    </>
+    </header>
   );
 };
 
