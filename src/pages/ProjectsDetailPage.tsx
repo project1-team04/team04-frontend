9f0ea6d05dc5d4e81671d24edc735ece53a4cee9
kwonboryong <kwonboryong@naver.com>
2025-01-21 19:10:19 +0900

    fix: MainLayout에서 서치바 컴포넌트 분리 - Search 컴포넌트와 Header를 프로젝트 상세 페이지 내부로 이동

--- src/pages/ProjectsDetailPage.tsx
+++ src/pages/ProjectsDetailPage.tsx
@@ -1,14 +1,35 @@
+import Header from '@/components/Header';
 import KanbanCard from '@/components/KanbanCard';
+import Search from '@/components/Search';
+import { Button } from '@/components/ui/button';
+
+const headerDetailTitle = 'Threadly';
 
 const ProjectsDetailPage = () => {
   return (
-    <>
-      <div className='ml-7 flex h-full gap-6'>
-        <KanbanCard status={'To Do'} issueCount={'4'} />
-        <KanbanCard status={'On Progress'} issueCount={'3'} />
-        <KanbanCard status={'Done'} issueCount={'2'} />
+    <div className='flex h-[90vh]'>
+      <aside className='flex w-[22%] flex-col border-r border-divider-default p-4'>
+        <Search />
+        <Button
+          className='w-24 text-xs'
+          variant='negative'
+          children='프로젝트 나가기'
+        />
+      </aside>
+
+      <div className='flex w-[78%] flex-col'>
+        <header className='flex h-28 items-center justify-between px-20'>
+          <Header children={headerDetailTitle} />
+          <Button variant='outline'>이슈 생성</Button>
+        </header>
+
+        <div className='flex h-full gap-6 px-6 pt-6'>
+          <KanbanCard status='To Do' issueCount='4' />
+          <KanbanCard status='On Progress' issueCount='3' />
+          <KanbanCard status='Done' issueCount='2' />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
