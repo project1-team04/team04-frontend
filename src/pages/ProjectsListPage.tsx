344ec744dd00d02544bab0b7f95cc278ecff770e
kwonboryong <kwonboryong@naver.com>
2025-01-21 21:40:50 +0900

    fix: Header, Button 컴포넌트 추가 및 레이아웃 조정

--- src/pages/ProjectsListPage.tsx
+++ src/pages/ProjectsListPage.tsx
@@ -7,35 +7,40 @@ import {
   PaginationItem,
   PaginationLink,
 } from '@/components/ui/pagination';
+import Header from '@/components/Header';
+import { Button } from '@/components/ui/button';
+
+const data = [
+  { id: '1', title: 'Threadly', issue: 1 },
+  { id: '2', title: 'Day6', issue: 2 },
+  { id: '3', title: 'Bee', issue: 3 },
+  { id: '4', title: 'Project X', issue: 4 },
+  { id: '5', title: 'Alpha', issue: 5 },
+  { id: '6', title: 'Beta', issue: 6 },
+  { id: '7', title: 'Gamma', issue: 7 },
+  { id: '8', title: 'Delta', issue: 8 },
+  { id: '9', title: 'Epsilon', issue: 9 },
+];
 
 const ProjectsListPage = () => {
-  const data = [
-    { id: '1', title: 'Threadly', issue: 1 },
-    { id: '2', title: 'Day6', issue: 2 },
-    { id: '3', title: 'Bee', issue: 3 },
-    { id: '4', title: 'Project X', issue: 4 },
-    { id: '5', title: 'Alpha', issue: 5 },
-    { id: '6', title: 'Beta', issue: 6 },
-    { id: '7', title: 'Gamma', issue: 7 },
-    { id: '8', title: 'Delta', issue: 8 },
-    { id: '9', title: 'Epsilon', issue: 9 },
-    // { id: '10', title: 'Beta', issue: 6 },
-    // { id: '11', title: 'Gamma', issue: 7 },
-    // { id: '12', title: 'Delta', issue: 8 },
-    // { id: '14', title: 'Epsilon', issue: 9 },
-  ];
-
   return (
-    <>
-      <div className='grid grid-cols-3 gap-5'>
-        {data.map((project) => (
-          <ProjectCardComponent
-            key={project.id}
-            title={project.title}
-            issue={project.issue}
-            onClick={() => console.log(`${project.title} 클릭`)}
-          />
-        ))}
+    <div className='m-auto w-[55%]'>
+      <div className='flex w-[78%] flex-col'>
+        <div className='flex h-28 items-center gap-5 px-20'>
+          <Header children={'내 프로젝트'} />
+          <Button variant='outline'>프로젝트 생성</Button>
+        </div>
+
+        <div className='grid grid-cols-3 gap-x-44 gap-y-5'>
+          {data.map((project) => (
+            <ProjectCardComponent
+              key={project.id}
+              title={project.title}
+              issue={project.issue}
+              onClick={() => console.log(`${project.title} 클릭`)}
+            />
+          ))}
+        </div>
       </div>
 
       <Pagination className='mt-6'>
@@ -51,7 +56,7 @@ const ProjectsListPage = () => {
           </PaginationItem>
         </PaginationContent>
       </Pagination>
-    </>
+    </div>
   );
 };
 
