49e8d95547a302dc185e13d6d66e5f8f2fc817bc
KimJeongHyun <ptr1129@hanmail.net>
2025-01-22 14:35:26 +0900

    fix: conflict

--- src/components/ProjectsLayout.tsx
+++ src/components/ProjectsLayout.tsx
@@ -15,18 +15,16 @@ interface ProjectLayoutProps {
 }
 
 const ProjectsLayout = ({ data }: ProjectLayoutProps) => {
-  const { modalType, open } = useModalStore();
-
   return (
     <div className='m-auto w-[50%]'>
-      <header className='flex items-center justify-between px-20 h-28'>
+      <header className='flex h-28 items-center justify-between px-20'>
         <Header children={'프로젝트 설정'} />
         <Button className='ml-auto' variant={'negative'} size={'sm'}>
           프로젝트 삭제
         </Button>
       </header>
 
-      <div className='flex items-center gap-5 ml-3'>
+      <div className='ml-3 flex items-center gap-5'>
         <p>프로젝트 이름</p>
         <div className='w-[84%]'>
           <Input placeholder="Enter Project's name" />
@@ -34,7 +32,7 @@ const ProjectsLayout = ({ data }: ProjectLayoutProps) => {
       </div>
 
       <div className='mt-4 h-[315px] overflow-auto bg-gray p-3'>
-        <div className='grid grid-cols-2 m-5 mt-3 align-items-center justify-items-center gap-y-4'>
+        <div className='align-items-center m-5 mt-3 grid grid-cols-2 justify-items-center gap-y-4'>
           {data.map((member) => (
             <MemberCard
               key={member.id}
@@ -46,11 +44,13 @@ const ProjectsLayout = ({ data }: ProjectLayoutProps) => {
         </div>
       </div>
 
-      <div className='flex flex-col gap-4 pb-5 mt-5'>
+      <div className='mt-5 flex flex-col gap-4 pb-5'>
         <Button
           variant='secondary'
           children={'+ 인원 추가'}
-          onClick={() => open('addPeople')}
+          onClick={() => {
+            console.log('인원 추가 버튼 클릭');
+          }}
         />
         <Button
           children={'생성 완료'}
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,25 +6,15 @@ const MainLayout = ({}) => {
     <>
       <div className='flex h-screen w-full flex-col'>
         <Navigation name={'권보령'} alt={'프로필 사진'} />
-
         <div className='flex flex-grow items-center justify-center'>
+          <main className='flex-grow'>
+            <Outlet />
+          </main>
           <main className='flex-grow'>
             <Outlet />
           </main>
         </div>
       </div>
-      {modalType === 'warning' && (
-        <Modal
-          title={'정말 삭제하시겠습니까?'}
-          content={'삭제 후에는 되돌리기가 불가능합니다.'}
-          icon={<RiErrorWarningLine className='h-[60px] w-[60px]' />}
-          css={'text-sm mt-[-18px]'}
-          buttons={[
-            { text: '아니오', variantStyle: 'outline' },
-            { text: '네', variantStyle: 'negative' },
-          ]}
-        />
-      )}
     </>
   );
 };
