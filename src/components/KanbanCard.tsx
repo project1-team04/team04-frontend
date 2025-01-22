1b31b59e6698472fb119301d331604016fcffad4
kwonboryong <kwonboryong@naver.com>
2025-01-22 10:17:48 +0900

    fix: variantStlyes 변수 함수 위로 분리

--- src/components/KanbanCard.tsx
+++ src/components/KanbanCard.tsx
@@ -5,48 +5,42 @@ interface KanbanCardProps {
   issueCount: string;
 }
 
+const variantStyles = {
+  'To Do': {
+    divideColor: 'divide-purple',
+    roundColor: 'bg-purple',
+  },
+  'On Progress': {
+    divideColor: 'divide-orange',
+    roundColor: 'bg-orange',
+  },
+  Done: {
+    divideColor: 'divide-green',
+    roundColor: 'bg-green',
+  },
+};
+
 // props: status, issueCount, data
 const KanbanCard = ({ status, issueCount }: KanbanCardProps) => {
-  const variantStyles = {
-    'To Do': {
-      divideColor: 'divide-purple',
-      roundColor: 'bg-purple',
-    },
-    'On Progress': {
-      divideColor: 'divide-orange',
-      roundColor: 'bg-orange',
-    },
-    Done: {
-      divideColor: 'divide-green',
-      roundColor: 'bg-green',
-    },
-  };
-
   const { divideColor, roundColor } = variantStyles[status];
 
   return (
-    <>
-      <div
-        className={`h-full w-[30%] divide-y-2 ${divideColor} overflow-auto rounded-t-2xl bg-bg-deep p-5`}
-      >
-        <div className='mb-3 flex h-9 items-center gap-4'>
-          <div className={`h-3 w-3 rounded-full ${roundColor}`} />
-          <span className='font-semibold'>{status}</span>
-          <span className='h-5 w-5 rounded-lg bg-border-default text-center text-sm'>
-            {issueCount}
-          </span>
-        </div>
-
-        <section className='flex max-h-[28rem] flex-col gap-5 overflow-auto pt-4'>
-          {/* data */}
-          <IssueCard
-            title={'Merge 이슈'}
-            issueId={'12345'}
-            manager={'권보령'}
-          />
-        </section>
+    <div
+      className={`h-full w-[30%] divide-y-2 ${divideColor} overflow-auto rounded-t-2xl bg-bg-deep p-5`}
+    >
+      <div className='mb-3 flex h-9 items-center gap-4'>
+        <div className={`h-3 w-3 rounded-full ${roundColor}`} />
+        <span className='font-semibold'>{status}</span>
+        <span className='h-5 w-5 rounded-lg bg-border-default text-center text-sm'>
+          {issueCount}
+        </span>
       </div>
-    </>
+
+      <section className='flex max-h-[28rem] flex-col gap-5 overflow-auto pt-4'>
+        {/* data */}
+        <IssueCard title={'Merge 이슈'} issueId={'12345'} manager={'권보령'} />
+      </section>
+    </div>
   );
 };
 
