09d0d978f352ca629467af29a60edcef097ab7b2
Hyerim Yang <hlhlstar@naver.com>
2025-01-23 11:10:37 +0900

    refactor: 프로젝트 생성 및 설정 레이아웃 개선

--- src/components/ProjectsLayout.tsx
+++ src/components/ProjectsLayout.tsx
@@ -16,49 +16,51 @@ interface ProjectLayoutProps {
 
 const ProjectsLayout = ({ data }: ProjectLayoutProps) => {
   return (
-    <div className='m-auto'>
-      <header className='flex h-28 items-center justify-between px-20'>
+    <div className='mx-auto flex w-1/2 flex-col'>
+      <header className='my-9 flex items-center justify-between gap-5'>
         <Header children={'프로젝트 설정'} />
-        <Button className='ml-auto' variant={'negative'} size={'sm'}>
+        <Button variant={'negative'} size={'sm'}>
           프로젝트 삭제
         </Button>
       </header>
 
       <div className='ml-3 flex items-center gap-5'>
         <p>프로젝트 이름</p>
-        <div className='w-[84%]'>
+        <div className='flex-1'>
           <Input placeholder="Enter Project's name" />
         </div>
       </div>
 
-      <div className='mt-4 h-[315px] overflow-auto bg-gray p-3'>
-        <div className='align-items-center m-5 mt-3 grid grid-cols-2 justify-items-center gap-y-4'>
-          {data.map((member) => (
-            <MemberCard
-              key={member.id}
-              name={member.name}
-              email={member.email}
-              position={member.position}
-            />
-          ))}
-        </div>
-      </div>
+      <main className='grow overflow-hidden'>
+        <div className='flex h-full flex-col'>
+          <div className='my-4 grid w-full grid-cols-2 gap-5 overflow-y-auto bg-bg-deep p-4'>
+            {data.map((member) => (
+              <MemberCard
+                key={member.id}
+                name={member.name}
+                email={member.email}
+                position={member.position}
+              />
+            ))}
+          </div>
 
-      <div className='mt-5 flex flex-col gap-4 pb-5'>
-        <Button
-          variant='secondary'
-          children={'+ 인원 추가'}
-          onClick={() => {
-            console.log('인원 추가 버튼 클릭');
-          }}
-        />
-        <Button
-          children={'생성 완료'}
-          onClick={() => {
-            console.log('생성 완료 버튼 클릭');
-          }}
-        />
-      </div>
+          <div className='mb-9 flex w-full flex-col gap-y-4'>
+            <Button
+              variant='secondary'
+              children={'+ 인원 추가'}
+              onClick={() => {
+                console.log('인원 추가 버튼 클릭');
+              }}
+            />
+            <Button
+              children={'생성 완료'}
+              onClick={() => {
+                console.log('생성 완료 버튼 클릭');
+              }}
+            />
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
--- src/pages/ProjectsSettingPage.tsx
+++ src/pages/ProjectsSettingPage.tsx
@@ -1,11 +1,28 @@
 import ProjectsLayout from '@/components/ProjectsLayout';
 
+interface Member {
+  id: string;
+  name: string;
+  email: string;
+  position?: 'Project Manager' | 'Member';
+}
+
+// 목 데이터
+const data: Member[] = [
+  {
+    id: '1',
+    name: '정태승',
+    email: 'hfgdf3@naver.com',
+    position: 'Project Manager',
+  },
+  { id: '2', name: '권보령', email: 'hfgdf3@naver.com' },
+  { id: '3', name: '양혜림', email: 'hfgdf3@naver.com' },
+  { id: '4', name: '이태정', email: 'hfgdf3@naver.com' },
+  { id: '5', name: '명광호', email: 'hfgdf3@naver.com', position: 'Member' },
+];
+
 const ProjectsSettingPage = () => {
-  return (
-    <>
-      <ProjectsLayout data={[]} />
-    </>
-  );
+  return <ProjectsLayout data={data} />;
 };
 
 export default ProjectsSettingPage;
