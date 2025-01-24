518fb845ee68cec98118d287e794281705dad4d1
Hyerim Yang <hlhlstar@naver.com>
2025-01-24 10:33:56 +0900

    refactor: 하드코딩된 로그인 방식을 폼 제출 시 API 호출 방식으로 변경

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -4,17 +4,24 @@ interface LoginResponse {
   accessToken: string;
 }
 
-export const login = async () => {
+// TODO) react-hook-form 적용 후 params 수정
+// TODO) 로그인 여부에 따른 라우팅
+export const login = async (
+  e: React.FormEvent<HTMLFormElement>,
+  email: string,
+  password: string
+) => {
+  e.preventDefault();
+
   try {
     const res = await instance.post<LoginResponse>('auth/login', {
-      email: 'hi563@naver.com',
-      password: '!a12345678',
+      email,
+      password,
     });
 
     localStorage.setItem('AccessToken', res.data.accessToken);
+    console.log('로그인 성공', res, res.data);
   } catch (error) {
     console.error('Login failed:', error);
   }
 };
-
-login();
--- src/pages/AuthLoginPage.tsx
+++ src/pages/AuthLoginPage.tsx
@@ -1,11 +1,12 @@
+import { useState } from 'react';
 import { Checkbox } from '@/components/ui/checkbox';
 import { paths } from '@/routers/paths';
 import ButtonComponent from '@/components/Button';
 import InputComponent from '@/components/Input';
 import AuthNavLinks from '@/components/AuthNavLinks';
-import NaverIcon from '../assets/btn_naver.svg?react';
-import KakaoIcon from '../assets/btn_kakao.svg?react';
-import GoogleIcon from '../assets/btn_google.svg?react';
+import NaverIcon from '@/assets/btn_naver.svg?react';
+import KakaoIcon from '@/assets/btn_kakao.svg?react';
+import GoogleIcon from '@/assets/btn_google.svg?react';
 import { login } from '@/apis/authApi';
 
 const socialLoginIcons = [
@@ -15,11 +16,32 @@ const socialLoginIcons = [
 ];
 
 const AuthLoginPage = () => {
+  // TODO) react-hook-form 적용하고 useState 제거
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className='mt-6 flex flex-col items-center gap-y-6'>
-      <form className='flex w-[250px] flex-col gap-2' onSubmit={() => login}>
-        <InputComponent type='email' placeholder='이메일' />
-        <InputComponent type='password' placeholder='비밀번호' />
+      {/* TODO) react-hook-form 적용 */}
+      <form
+        className='flex w-[250px] flex-col gap-2'
+        onSubmit={(e) => {
+          e.preventDefault();
+          login(e, email, password);
+        }}
+      >
+        <InputComponent
+          type='email'
+          placeholder='이메일'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <InputComponent
+          type='password'
+          placeholder='비밀번호'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <div className='my-2 flex items-center gap-2'>
           <Checkbox id='rememberId' />
@@ -31,7 +53,7 @@ const AuthLoginPage = () => {
           </label>
         </div>
 
-        <ButtonComponent>로그인</ButtonComponent>
+        <ButtonComponent type='submit'>로그인</ButtonComponent>
       </form>
 
       <div className='flex w-full items-center text-xs text-text-disabled before:me-6 before:flex-1 before:border-t before:border-border-default after:ms-6 after:flex-1 after:border-t after:border-border-default'>
