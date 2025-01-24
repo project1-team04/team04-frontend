4060b58579d0abfbaec26b4506c53f3438cdaeb0
Hyerim Yang <hlhlstar@naver.com>
2025-01-24 11:18:37 +0900

    feat: 로그아웃 기능 추가 및 api 연동

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -5,7 +5,8 @@ interface LoginResponse {
 }
 
 // TODO) react-hook-form 적용 후 params 수정
-// TODO) 로그인 여부에 따른 라우팅
+// TODO) 로그인 여부에 따른 라우팅 로직 추가
+// /projects로 redirect
 export const login = async (
   e: React.FormEvent<HTMLFormElement>,
   email: string,
@@ -23,5 +24,33 @@ export const login = async (
     console.log('로그인 성공', res, res.data);
   } catch (error) {
     console.error('Login failed:', error);
+  }
+};
+
+// TODO) 로그인 여부에 따른 라우팅 로직 추가
+// /auth/login으로 redirect
+export const logout = async () => {
+  const accessToken = localStorage.getItem('AccessToken');
+
+  if (!accessToken) {
+    console.log('Access Token 없음. 로그인 상태가 아님.', accessToken);
+    return;
+  } else {
+    console.log('로그인 상태', accessToken);
+  }
+
+  try {
+    await instance.post(
+      '/auth/logout',
+      {},
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+
+    localStorage.removeItem('AccessToken');
+    console.log('로그아웃 완료');
+  } catch (error) {
+    console.error('Logout failed:', error);
   }
 };
