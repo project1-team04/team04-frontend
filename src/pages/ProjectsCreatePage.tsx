f8573384b12ab28dda92b9f7785ef8409e68f9a3
kwonboryong <kwonboryong@naver.com>
2025-01-24 16:23:22 +0900

    feat: 프로젝트 생성 api 및 state 추가 - ProjectsLayout에 버튼 클릭 핸들러 전달달

--- src/pages/ProjectsCreatePage.tsx
+++ src/pages/ProjectsCreatePage.tsx
@@ -1,4 +1,7 @@
+import instance from '@/apis/instance';
 import ProjectsLayout from '@/components/ProjectsLayout';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface Member {
   id: string;
@@ -15,14 +18,41 @@ const data: Member[] = [
     email: 'hfgdf3@naver.com',
     position: 'Project Manager',
   },
-  { id: '2', name: '권보령', email: 'hfgdf3@naver.com' },
-  { id: '3', name: '양혜림', email: 'hfgdf3@naver.com' },
-  { id: '4', name: '이태정', email: 'hfgdf3@naver.com' },
-  { id: '5', name: '명광호', email: 'hfgdf3@naver.com', position: 'Member' },
+  // { id: '2', name: '권보령', email: 'hfgdf3@naver.com' },
+  // { id: '3', name: '양혜림', email: 'hfgdf3@naver.com' },
+  // { id: '4', name: '이태정', email: 'hfgdf3@naver.com' },
+  // { id: '5', name: '명광호', email: 'hfgdf3@naver.com', position: 'Member' },
 ];
 
 const ProjectsCreatePage = () => {
-  return <ProjectsLayout data={data} header='프로젝트 생성' />;
+  const [projectName, setProjectName] = useState('');
+  const navigate = useNavigate();
+
+  const createProject = async () => {
+    try {
+      const res = await instance.post('/projects', {
+        name: projectName,
+      });
+      const { id, name } = res.data;
+      console.log('프로젝트 생성:', res.data);
+      navigate(`/projects/details?projectId=${id}`, {
+        state: {
+          projectDetails: { id, name },
+        },
+      });
+    } catch (error) {
+      console.log('프로젝트 생성 에러: ', error);
+    }
+  };
+
+  return (
+    <ProjectsLayout
+      data={data}
+      header='프로젝트 생성'
+      onInputChange={(e) => setProjectName(e.target.value)}
+      onCreate={createProject}
+    />
+  );
 };
 
 export default ProjectsCreatePage;
