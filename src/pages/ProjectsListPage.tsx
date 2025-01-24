be7a35fedc225c4dc21a25099dcaed6b34d82001
kwonboryong <kwonboryong@naver.com>
2025-01-24 14:56:49 +0900

    feat: 프로젝트 목록 패칭 코드 추가 - 목 데이터 및 주석 삭제

--- src/pages/ProjectsListPage.tsx
+++ src/pages/ProjectsListPage.tsx
@@ -9,37 +9,40 @@ import {
 import { Button } from '@/components/ui/button';
 import { MdKeyboardArrowLeft } from 'react-icons/md';
 import { MdKeyboardArrowRight } from 'react-icons/md';
-
-const data = [
-  { id: '1', title: 'Threadly', issue: 1 },
-  { id: '2', title: 'Day6', issue: 2 },
-  { id: '3', title: 'Bee', issue: 3 },
-  { id: '4', title: 'Project X', issue: 4 },
-  { id: '5', title: 'Alpha', issue: 5 },
-  { id: '6', title: 'Beta', issue: 6 },
-  { id: '7', title: 'Gamma', issue: 7 },
-  { id: '8', title: 'Delta', issue: 8 },
-  { id: '9', title: 'Epsilon', issue: 9 },
-];
+import { useEffect, useState } from 'react';
+import { getUserProjects } from '@/apis/projectApi';
 
 const ProjectsListPage = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const projectData = await getUserProjects();
+        setProjects(projectData);
+      } catch (error) {
+        console.log('fetch 에러', error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
   return (
-    <div className='mx-auto flex flex-col'>
-      <header className='my-9 flex items-center gap-5'>
+    <div className='flex flex-col mx-auto'>
+      <header className='flex items-center gap-5 my-9'>
         <Header children={'내 프로젝트'} />
         <Button variant='outline'>프로젝트 생성</Button>
       </header>
 
-      {/* main 태그 없으면 Pagination의 margin-bottom이 무시됨 */}
-      {/* flex 컨테이너 내부에서 자식 요소의 margin-bottom이 유지되도록 하기 위해 main 태그 추가 */}
       <main>
         <div className='grid grid-cols-3 gap-5'>
-          {data.map((project) => (
+          {projects.map((project) => (
             <ProjectCard
               key={project.id}
-              title={project.title}
-              issue={project.issue}
-              onClick={() => console.log(`${project.title} 클릭`)}
+              title={project.name}
+              issue={project.issueCount}
+              onClick={() => console.log(`${project.name} 클릭`)}
             />
           ))}
         </div>
