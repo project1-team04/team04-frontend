600e3e07f9694c481ada4188b81b2db0451d1935
kwonboryong <kwonboryong@naver.com>
2025-01-25 15:57:48 +0900

    fix: 이슈 버튼 드롭다운으로 수정

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -1,4 +1,4 @@
-import { Button } from './ui/button';
+import DropDown from './DropDown';
 
 export interface IssueCardProps {
   id: string;
@@ -7,15 +7,36 @@ export interface IssueCardProps {
 }
 
 const IssueCard = ({ title, id, assignee }: IssueCardProps) => {
+  const dropdownItems = [
+    {
+      label: 'None',
+      onClick: () => console.log('None'),
+    },
+    {
+      label: 'Feature',
+      onClick: () => console.log('Feature'),
+    },
+    {
+      label: 'Bug',
+      onClick: () => console.log('Bug'),
+    },
+    {
+      label: 'Edit labels',
+      onClick: () => console.log('Edit labels'),
+    },
+  ];
+
   return (
     // FIX) title, id, assignee 길어지는 경우 대응
     <div className='grid h-32 w-full gap-6 rounded-lg bg-bg p-5'>
       <span className='m-1 text-lg font-semibold'>{title}</span>
       <div className='flex items-center justify-between text-sm'>
         <div className='flex items-center gap-2'>
-          <Button variant={'outline'} size={'sm'} className='border-none p-1'>
-            <div className='h-3 w-3 rounded-full bg-red' />
-          </Button>
+          <DropDown
+            items={dropdownItems}
+            className='h-2 rounded-full bg-red p-2'
+          />
+
           <span>{id}</span>
         </div>
         <span>{assignee}</span>
