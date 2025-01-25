d369e5244fd673988bb62031bdec0926186a1fe0
kwonboryong <kwonboryong@naver.com>
2025-01-25 15:33:24 +0900

    style: title margin 추가

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -9,8 +9,8 @@ export interface IssueCardProps {
 const IssueCard = ({ title, id, assignee }: IssueCardProps) => {
   return (
     // FIX) title, id, assignee 길어지는 경우 대응
-    <div className='grid h-32 w-full gap-6 rounded-lg bg-bg p-4'>
-      <span className='text-lg font-semibold'>{title}</span>
+    <div className='grid h-32 w-full gap-6 rounded-lg bg-bg p-5'>
+      <span className='m-1 text-lg font-semibold'>{title}</span>
       <div className='flex items-center justify-between text-sm'>
         <div className='flex items-center gap-2'>
           <Button variant={'outline'} size={'sm'} className='border-none p-1'>
