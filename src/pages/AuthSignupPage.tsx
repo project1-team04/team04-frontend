e3cfd71abfb4577f9d7e3636be8ab75dce210430
Hyerim Yang <hlhlstar@naver.com>
2025-01-27 16:07:49 +0900

    feat: 회원가입 폼에 react-hook-form 적용
    - TODO: confirmPassword 유효성 검사 추가, 이메일 인증 버튼 클릭 여부 확인 필요

--- src/pages/AuthSignupPage.tsx
+++ src/pages/AuthSignupPage.tsx
@@ -1,16 +1,67 @@
+import { useForm, useWatch } from 'react-hook-form';
 import { paths } from '@/routers/paths';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import AuthNavLinks from '@/components/AuthNavLinks';
 
+type SignupFormInputs = {
+  name: string;
+  email: string;
+  emailCode: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const AuthSignupPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitted, isSubmitting },
+  } = useForm<SignupFormInputs>();
+
+  const onSubmit = (data: SignupFormInputs) => {
+    console.log(data);
+  };
+
+  const passwordErrorMessage =
+    '비밀번호는 8~16자 길이로, 영문 대소문자, 숫자, 특수문자(@, !)를 각각 최소 1개 이상 포함해야 합니다.';
+
   return (
     <div className='mt-6 flex flex-col items-center gap-y-6'>
-      <form className='flex w-[250px] flex-col gap-2'>
-        <Input placeholder='이름' />
+      <form
+        className='flex w-[250px] flex-col gap-2'
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate // 기본 HTML5 검증 메세지 제거
+      >
+        <Input
+          placeholder='이름'
+          aria-invalid={
+            isSubmitted ? (errors.name ? 'true' : 'false') : undefined
+          }
+          {...register('name', {
+            required: '이름을 입력해주세요.',
+          })}
+        />
+        {errors.name && (
+          <small role='alert' className='text-text-error'>
+            {errors.name.message?.toString()}
+          </small>
+        )}
+
+        {/* TODO) '인증' 버튼 눌렀는지 확인, 버튼 클릭 여부에 따라 label 다르게 표시 */}
         <Input
           type='email'
           placeholder='이메일'
+          aria-invalid={
+            isSubmitted ? (errors.email ? 'true' : 'false') : undefined
+          }
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+              message: '이메일 형식에 맞지 않습니다.',
+            },
+          })}
           iconPosition='right'
           icon={
             <Button variant='outline' className='w-auto'>
@@ -18,9 +69,22 @@ const AuthSignupPage = () => {
             </Button>
           }
         />
+        {errors.email && (
+          <small role='alert' className='text-text-error'>
+            {errors.email.message?.toString()}
+          </small>
+        )}
+
+        {/* TODO) '확인' 버튼 눌렀는지 확인 */}
         <Input
-          type='email'
           placeholder='이메일 인증번호'
+          aria-invalid={
+            isSubmitted ? (errors.emailCode ? 'true' : 'false') : undefined
+          }
+          {...register('emailCode', {
+            required:
+              '메일로 전송된 인증번호를 입력하고 확인 버튼을 눌러주세요.',
+          })}
           iconPosition='right'
           icon={
             <Button variant='outline' className='w-auto'>
@@ -28,10 +92,64 @@ const AuthSignupPage = () => {
             </Button>
           }
         />
-        <Input placeholder='이름' />
-        <Input type='password' placeholder='비밀번호' />
-        <Input type='password' placeholder='비밀번호 확인' />
-        <Button>회원가입</Button>
+        {errors.emailCode && (
+          <small role='alert' className='text-text-error'>
+            {errors.emailCode.message?.toString()}
+          </small>
+        )}
+
+        <Input
+          type='password'
+          placeholder='비밀번호'
+          aria-invalid={
+            isSubmitted ? (errors.password ? 'true' : 'false') : undefined
+          }
+          {...register('password', {
+            required: '비밀번호를 입력해주세요.',
+            minLength: {
+              value: 8,
+              message: passwordErrorMessage,
+            },
+            maxLength: {
+              value: 16,
+              message: passwordErrorMessage,
+            },
+            pattern: {
+              value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@!])[a-zA-Z\d@!]{8,16}$/,
+              message: passwordErrorMessage,
+            },
+          })}
+        />
+        {errors.password && (
+          <small role='alert' className='text-text-error'>
+            {errors.password.message?.toString()}
+          </small>
+        )}
+
+        {/* TODO) password의 입력값과 같은지 확인 */}
+        <Input
+          type='password'
+          placeholder='비밀번호 확인'
+          aria-invalid={
+            isSubmitted
+              ? errors.confirmPassword
+                ? 'true'
+                : 'false'
+              : undefined
+          }
+          {...register('confirmPassword', {
+            required: '비밀번호를 입력해주세요.',
+          })}
+        />
+        {errors.confirmPassword && (
+          <small role='alert' className='text-text-error'>
+            {errors.confirmPassword.message?.toString()}
+          </small>
+        )}
+
+        <Button type='submit' disabled={isSubmitting}>
+          회원가입
+        </Button>
       </form>
 
       <AuthNavLinks
