050dcff65fa676c97fc509b449e3e7ae5acd6809
Hyerim Yang <hlhlstar@naver.com>
2025-01-27 16:23:07 +0900

    feat: 비밀번호 찾기 폼에 react-hook-form 적용 및 이메일 유효성 검사 추가

--- src/pages/AuthFindPasswordPage.tsx
+++ src/pages/AuthFindPasswordPage.tsx
@@ -1,19 +1,60 @@
+import { useForm } from 'react-hook-form';
 import { paths } from '@/routers/paths';
-import ButtonComponent from '@/components/Button';
-import InputComponent from '@/components/Input';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
 import AuthNavLinks from '@/components/AuthNavLinks';
 
 const AuthForgotPasswordPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitted, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div className='mt-6 flex flex-col items-center gap-y-6'>
       {/* TODO) 폼 제출 후 바로 로그인 페이지로 리다이렉트, 임시 비밀번호로 로그인하면 바로 마이페이지로 리다이렉트 */}
-      {/* TODO) 이메일이 등록된 계정인지 확인 후 폼 제출할 수 있게 할 것인지 논의 */}
       <p className='text-sm text-text-sub'>
         가입하신 이메일로 임시 비밀번호를 보내드릴게요.
       </p>
-      <form className='flex w-[250px] flex-col gap-2'>
-        <InputComponent type='email' placeholder='이메일' />
-        <ButtonComponent>임시 비밀번호 전송</ButtonComponent>
+      <form
+        className='flex w-[250px] flex-col gap-2'
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate // 기본 HTML5 검증 메세지 제거
+      >
+        <Input
+          placeholder='이메일'
+          aria-invalid={
+            isSubmitted ? (errors.email ? 'true' : 'false') : undefined
+          }
+          {...register('email', {
+            required:
+              '먼저 가입된 이메일인지 확인이 필요해요. 확인 버튼을 눌러주세요.',
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+              message: '이메일 형식에 맞지 않습니다.',
+            },
+          })}
+          iconPosition='right'
+          icon={
+            <Button variant='outline' className='w-auto'>
+              확인
+            </Button>
+          }
+        />
+        {errors.email && (
+          <small role='alert' className='text-text-error'>
+            {errors.email.message?.toString()}
+          </small>
+        )}
+
+        <Button type='submit' disabled={isSubmitting}>
+          임시 비밀번호 전송
+        </Button>
       </form>
 
       <AuthNavLinks
