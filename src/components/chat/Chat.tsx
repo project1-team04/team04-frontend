f5f301849e93bc0930a9b76324bbbe0409b382cc
kwonboryong <kwonboryong@naver.com>
2025-01-28 21:25:40 +0900

    style: 채팅 프로필 컴포넌트 추가

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,11 +1,14 @@
 import ChatMessageInput from './ChatMessageInput';
+import ChatProfile from './ChatProfile';
 
 const Chat = () => {
   return (
     <div className='flex h-full w-full flex-col bg-gray-50'>
       <div className='mx-5 flex h-[10%] items-center justify-between border-b-[0.5px] border-border-default'>
         <p className='font-semibold'>이슈명</p>
-        <div className='bg-black'>프로필(멤버들)</div>
+        <div className='flex'>
+          <ChatProfile />
+        </div>
       </div>
       <div className='mx-5 h-[80%] border-b-[0.5px] border-border-default p-5'>
         채팅룸
