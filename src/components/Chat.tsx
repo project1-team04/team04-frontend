221cbd1c2c27679cb7cbede0e392acc44500af57
kwonboryong <kwonboryong@naver.com>
2025-01-28 20:31:55 +0900

    style: 채팅방 레이아웃 초기 설정

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,14 @@
 const Chat = () => {
-  return <div></div>;
+  return (
+    <div className='flex h-full w-full flex-col bg-gray-400'>
+      <div className='flex h-[10%] items-center justify-between'>
+        <p className='bg-red'>이슈명</p>
+        <div className='bg-black'>프로필(멤버들)</div>
+      </div>
+      <div className='h-[77%] bg-orange'>채팅룸</div>
+      <div className='h-[13%] bg-yellow-400'>채팅 입력칸</div>
+    </div>
+  );
 };
 
 export default Chat;
