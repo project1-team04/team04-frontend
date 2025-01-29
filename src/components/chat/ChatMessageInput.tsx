11cd3ecb8cc0a0ab97fe3de6ac39c1ec653f1ac0
kwonboryong <kwonboryong@naver.com>
2025-01-29 15:38:16 +0900

    feat: 채팅 메시지 입력 컴포넌트에서 메시지 전송 기능 및 enter로 전송 기능 추가

--- src/components/chat/ChatMessageInput.tsx
+++ src/components/chat/ChatMessageInput.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 
-const ChatMessageInput = () => {
+interface ChatMessageInputProps {
+  onSendMessage: (message: string) => void;
+}
+
+const ChatMessageInput: React.FC<ChatMessageInputProps> = ({
+  onSendMessage,
+}) => {
+  const [message, setMessage] = useState('');
+
+  const handleSend = () => {
+    if (!message.trim()) return;
+    onSendMessage(message);
+    setMessage('');
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSend();
+    }
+  };
+
   return (
     <div className='flex h-full items-center justify-center gap-3 p-5'>
-      <Input placeholder='Write message' />
-      <Button>Send</Button>
+      <Input
+        placeholder='Write message'
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={handleKeyPress}
+      />
+      <Button onClick={handleSend}>Send</Button>
     </div>
   );
 };
