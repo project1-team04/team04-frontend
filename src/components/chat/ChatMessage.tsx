b93a06eaba154f345fd983933e705b385c5c5d3a
kwonboryong <kwonboryong@naver.com>
2025-01-29 15:35:59 +0900

    style: 채팅 메시지 컴포넌트 값 추가

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,32 +1,43 @@
-const ChatMessage = () => {
+import { useState, useEffect } from 'react';
+
+const ChatMessage = ({ nickname, chatting, time, isMe }: any) => {
+  const [chatNick, setChatNick] = useState('');
+
+  useEffect(() => {
+    setChatNick(isMe ? '나' : nickname);
+  }, [isMe, nickname]);
+
   return (
     <>
-      <div id='me' className='my-5 flex justify-end'>
-        <div id='time' className='mr-3 flex flex-col justify-end'>
-          <p className='text-xs text-black'>time</p>
-        </div>
-        <div className='mr-3'>
-          <div id='nickname' className='flex justify-end'>
-            <p className='text-xs text-black'>chatNick</p>
+      {isMe ? (
+        <div id='me' className='my-5 flex justify-end'>
+          <div id='time' className='mr-3 flex flex-col justify-end'>
+            <p className='text-xs text-black'>{time}</p>
           </div>
-          <div id='chatting' className='mt-2 rounded-md bg-blue-500 p-3'>
-            <span className='text-xs text-white'>chatting</span>
-          </div>
-        </div>
-      </div>
-      <div id='you' className='my-5 flex'>
-        <div className='ml-3'>
-          <p id='nickname' className='text-xs text-black'>
-            chatNick
-          </p>
-          <div id='chatting' className='mt-2 rounded-md bg-gray-200 p-3'>
-            <span className='text-xs text-black'>chatting</span>
+          <div className='mr-3'>
+            <div id='nickname' className='flex justify-end'>
+              <p className='text-xs text-black'>{chatNick}</p>
+            </div>
+            <div id='chatting' className='mt-2 rounded-md bg-blue-500 p-3'>
+              <span className='text-xs text-white'>{chatting}</span>
+            </div>
           </div>
         </div>
-        <div id='time' className='ml-3 flex flex-col justify-end'>
-          <p className='text-xs text-black'>time</p>
+      ) : (
+        <div id='you' className='my-5 flex'>
+          <div className='ml-3'>
+            <p id='nickname' className='text-xs text-black'>
+              {chatNick}
+            </p>
+            <div id='chatting' className='mt-2 rounded-md bg-gray-200 p-3'>
+              <span className='text-xs text-black'>{chatting}</span>
+            </div>
+          </div>
+          <div id='time' className='ml-3 flex flex-col justify-end'>
+            <p className='text-xs text-black'>{time}</p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
