5f25004893395d35ae81552b897f31053b7a961b
Hyerim Yang <hlhlstar@naver.com>
2025-01-30 22:55:18 +0900

    feat: 사진, 이름 변경 UI 퍼블리싱

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -2,14 +2,23 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { paths } from '@/routers/paths';
 import EditPasswordForm from '@/components/EditPasswordForm';
+import EditProfileInfo from '@/components/EditProfileInfo';
 import Button from '@/components/Button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { RiEdit2Line } from 'react-icons/ri';
 import { IoChevronForward } from 'react-icons/io5';
 
 const ProfilePage = () => {
   const navigate = useNavigate();
+  const [isEditingProfileInfo, setIsEditingProfileInfo] = useState(false);
   const [isEditingPassword, setIsEditingPassword] = useState(false);
 
+  // 사진, 이름 변경
+  if (isEditingProfileInfo) {
+    return <EditProfileInfo onClose={() => setIsEditingProfileInfo(false)} />;
+  }
+
+  // 비밀번호 변경
   if (isEditingPassword) {
     return <EditPasswordForm onClose={() => setIsEditingPassword(false)} />;
   }
@@ -18,13 +27,28 @@ const ProfilePage = () => {
     <div className='div m-auto grid w-1/3 max-w-[450px] divide-y-2 divide-divider-default rounded-2xl bg-bg-deep px-4'>
       <div className='flex w-full flex-col items-center gap-2 pb-4 pt-8'>
         {/* TODO) UserStore에서 사용자 정보 가져오기 */}
-        {/* TODO) 프로필 사진, 이름 변경 버튼 추가 */}
-        <Avatar className='h-20 w-20'>
-          <AvatarImage />
-          <AvatarFallback />
-        </Avatar>
+        <div
+          className='relative inline-block cursor-pointer'
+          onClick={() => setIsEditingProfileInfo(true)}
+        >
+          <Avatar className='h-20 w-20'>
+            <AvatarImage />
+            <AvatarFallback />
+          </Avatar>
+          <RiEdit2Line
+            className='absolute bottom-0 right-0 rounded-full bg-white p-1 text-text-sub shadow-md'
+            size={20}
+            onClick={() => setIsEditingProfileInfo(true)}
+          />
+        </div>
         <div className='flex flex-col items-center'>
-          <p className='text-xl font-bold'>양혜림</p>
+          <p className='flex items-center gap-1 text-xl font-bold'>
+            양혜림
+            <RiEdit2Line
+              className='cursor-pointer text-text-sub'
+              onClick={() => setIsEditingProfileInfo(true)}
+            />
+          </p>
           <p className='text-text-sub'>hlhlstar@naver.com</p>
         </div>
       </div>
