980444610fc563855b03700c500f091fb5b80c3f
Hyerim Yang <hlhlstar@naver.com>
2025-01-30 18:20:25 +0900

    feat: 비밀번호 변경 UI 퍼블리싱

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { paths } from '@/routers/paths';
+import EditProfile from '@/components/EditProfile';
 import Button from '@/components/Button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { IoChevronForward } from 'react-icons/io5';
 
 const ProfilePage = () => {
+  const navigate = useNavigate();
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (isEditing) {
+    return <EditProfile onCancel={() => setIsEditing(false)} />;
+  }
+
   return (
     <div className='div m-auto grid w-1/3 max-w-[450px] divide-y-2 divide-divider-default rounded-2xl bg-bg-deep px-4'>
       <div className='flex w-full flex-col items-center gap-2 pb-4 pt-8'>
@@ -18,8 +29,10 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className='flex w-full flex-col gap-2 py-4'>
-        <Button>비밀번호 변경</Button>
-        <Button>프로젝트 권한 위임</Button>
+        <Button onClick={() => setIsEditing(true)}>비밀번호 변경</Button>
+        <Button onClick={() => navigate(paths.profile.delegate.fullPath)}>
+          프로젝트 권한 위임
+        </Button>
       </div>
       <div className='flex cursor-pointer items-center justify-end gap-1 py-4 text-sm text-text-sub'>
         <p>회원탈퇴</p>
