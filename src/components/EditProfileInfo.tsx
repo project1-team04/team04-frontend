c3467f625d5277dd6ae43310d8b272e1ed9031a1
Hyerim Yang <hlhlstar@naver.com>
2025-02-01 15:28:11 +0900

    feat: 프로필 관련 페이지 UI 수정

--- src/components/EditProfileInfo.tsx
+++ src/components/EditProfileInfo.tsx
@@ -1,6 +1,8 @@
 import Button from './Button';
 import Input from './Input';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { RiUpload2Line } from 'react-icons/ri';
+import { RiDeleteBin5Line } from 'react-icons/ri';
 
 interface EditProfileInfoProps {
   onClose: () => void;
@@ -8,31 +10,42 @@ interface EditProfileInfoProps {
 
 const EditProfileInfo = ({ onClose }: EditProfileInfoProps) => {
   return (
-    <div className='div m-auto flex w-1/3 min-w-min max-w-[450px] flex-col items-center gap-8 rounded-2xl bg-bg-deep px-4 py-8'>
-      <Avatar className='h-32 w-32'>
-        <AvatarImage />
-        <AvatarFallback />
-      </Avatar>
-
-      {/* TODO) defalutValue로 값 가져오기 */}
+    <div className='div m-auto flex w-[300px] min-w-min flex-col gap-8 rounded-2xl bg-bg-deep p-4'>
+      {/* TODO) defalutValue로 기존 사진과 이름 가져오기 */}
       <form
-        className='flex w-full items-center gap-2'
+        className='flex w-full flex-col items-center gap-y-4'
         noValidate // 기본 HTML5 검증 메세지 제거
       >
-        <label>이름</label>
-        {/* FIXME) input 안늘어남 */}
-        <Input className='grow' />
-      </form>
+        <div className='relative my-4 inline-block'>
+          <Avatar className='h-32 w-32'>
+            <AvatarImage />
+            <AvatarFallback />
+          </Avatar>
+          <div className='absolute bottom-0 right-0 flex items-center divide-x-2 rounded-full bg-white shadow-md'>
+            <button className='flex items-center justify-center rounded-l-full p-2 hover:bg-gray-hover'>
+              <RiDeleteBin5Line />
+            </button>
+            <button className='flex items-center justify-center rounded-r-full p-2 hover:bg-gray-hover'>
+              <RiUpload2Line />
+            </button>
+          </div>
+        </div>
 
-      <div className='flex w-full gap-4'>
-        <Button
-          children={'취소'}
-          variant='outline'
-          onClick={onClose}
-          className='flex-1 bg-bg'
-        />
-        <Button children={'변경하기'} type='submit' className='flex-1' />
-      </div>
+        <div className='flex w-full items-center gap-2'>
+          <label className='flex-none'>이름</label>
+          <Input />
+        </div>
+
+        <div className='mt-4 flex min-w-full gap-4'>
+          <Button
+            children={'취소'}
+            variant='outline'
+            onClick={onClose}
+            className='flex-1 bg-bg'
+          />
+          <Button children={'변경하기'} type='submit' className='flex-1' />
+        </div>
+      </form>
     </div>
   );
 };
