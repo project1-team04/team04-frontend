6bca56feaded908be3b7a2c1afc00d7b74010b52
Hyerim Yang <hlhlstar@naver.com>
2025-02-01 15:50:07 +0900

    refactor: 불필요한 Fragment 제거

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,32 +22,30 @@ const Modal = ({ title, content, icon, css, buttons }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <>
-      <div className='fixed inset-0 z-50 flex items-center justify-center'>
-        <Backdrop onClick={close} />
-        <div className='flex h-[300px] w-[400px] flex-col items-center justify-center bg-bg-light'>
-          {icon && <>{icon}</>}
+    <div className='fixed inset-0 z-50 flex items-center justify-center'>
+      <Backdrop onClick={close} />
+      <div className='flex h-[300px] w-[400px] flex-col items-center justify-center bg-bg-light'>
+        {icon && <>{icon}</>}
 
-          <p className='m-5 text-2xl font-semibold'>{title}</p>
-          <div className={css}>{content}</div>
+        <p className='m-5 text-2xl font-semibold'>{title}</p>
+        <div className={css}>{content}</div>
 
-          <div className='m-5 flex gap-3'>
-            {buttons.map((button, index) => (
-              <Button
-                key={index}
-                variant={button.variantStyle || 'primary'}
-                onClick={() => {
-                  button.onClick();
-                  close();
-                }}
-              >
-                {button.text}
-              </Button>
-            ))}
-          </div>
+        <div className='m-5 flex gap-3'>
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              variant={button.variantStyle || 'primary'}
+              onClick={() => {
+                button.onClick();
+                close();
+              }}
+            >
+              {button.text}
+            </Button>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
