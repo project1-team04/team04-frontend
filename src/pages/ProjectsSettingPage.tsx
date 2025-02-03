f89d9f549a6d510e1821d4cfe4376075e1a15aea
kwonboryong <kwonboryong@naver.com>
2025-02-03 17:27:31 +0900

    refactor: projectLayout에서 분리한 코드 추가 - 멤버 카드 동적 생성 - 인원 초대 기능 - 인원 초대 모달 및 input 경고 문구 출력 - 프로젝트명 수정 기능

--- src/pages/ProjectsSettingPage.tsx
+++ src/pages/ProjectsSettingPage.tsx
@@ -1,7 +1,12 @@
 import { deleteProject, inviteMember, modifyProject } from '@/apis/projectApi';
+import MemberCard from '@/components/MemberCard';
+import Modal from '@/components/Modal';
 import ProjectsLayout from '@/components/ProjectsLayout';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useModalStore, ModalType } from '@/stores/useModalStore';
 
 interface Member {
   id: string;
@@ -12,6 +17,7 @@ interface Member {
 
 const ProjectsSettingPage = () => {
   const navigate = useNavigate();
+  const { modalType, open } = useModalStore();
 
   const [projectName, setProjectName] = useState('');
   const [email, setEmail] = useState<string>('');
@@ -65,25 +71,83 @@ const ProjectsSettingPage = () => {
   };
 
   return (
-    <>
-      <ProjectsLayout
-        header='프로젝트 설정'
-        deleteButton='프로젝트 삭제'
-        member={members}
-        projectName={projectName}
-        projectId={projectId ? Number(projectId) : undefined}
-        onUpdate={handleUpdate}
-        onInputChange={(e) => setProjectName(e.target.value)}
-        onDelete={handleDelete}
-        onInvite={handleInviteMember}
-        onEmailChange={(e) => {
-          setEmail(e.target.value);
-          setInviteMessage('');
-        }}
-        email={email}
-        inviteMessage={inviteMessage}
-      />
-    </>
+    <ProjectsLayout
+      header='프로젝트 설정'
+      deleteButton='프로젝트 삭제'
+      onDelete={handleDelete}
+      projectId={Number(projectId)}
+      onInputChange={(e) => setProjectName(e.target.value)}
+    >
+      <div className='flex h-full flex-col'>
+        <div className='mb-4 mt-4 grid w-full grid-cols-2 gap-5 overflow-y-auto bg-bg-deep p-4'>
+          {members.map((member) => (
+            <MemberCard
+              key={member.id}
+              name={member.name}
+              email={member.email}
+              position={member.position}
+            />
+          ))}
+        </div>
+        {inviteMessage && (
+          <p className='mb-4 mt-[-10px] text-sm text-text-error'>
+            {inviteMessage}
+          </p>
+        )}
+
+        <div className='mb-9 flex w-full flex-col gap-y-4'>
+          <Button
+            variant='secondary'
+            children={'+ 인원 추가'}
+            onClick={() => open(ModalType.INVITE_PEOPLE)}
+          />
+
+          <Button
+            children='설정 완료'
+            disabled={!projectName}
+            onClick={() => handleUpdate(Number(projectId))}
+          />
+        </div>
+      </div>
+
+      {modalType === ModalType.INVITE_PEOPLE && (
+        <Modal
+          title={'인원 초대'}
+          content={
+            <div className='w-64'>
+              <span className='text-sm'>
+                이메일 <span className='text-red'>*</span>
+              </span>
+              <Input
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setInviteMessage('');
+                }}
+              />
+            </div>
+          }
+          buttons={[
+            { text: '취소', variantStyle: 'outline' },
+            {
+              text: '추가',
+              onClick: () => {
+                if (projectId) {
+                  console.log('인원 추가 전송:', projectId, email);
+
+                  if (email) {
+                    handleInviteMember(Number(projectId), email);
+                  } else {
+                    console.error('이메일이 비어 있습니다.');
+                  }
+                } else {
+                  console.error('projectId가 undefined입니다.');
+                }
+              },
+            },
+          ]}
+        />
+      )}
+    </ProjectsLayout>
   );
 };
 
