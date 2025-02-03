7673d8e50f4c2d38ee1b24ea3707b7d5a2c3215b
Hyerim Yang <hlhlstar@naver.com>
2025-02-03 17:43:54 +0900

    feat: JWT 디코딩 및 userId 반환
    - `decodeToken.ts`: JWT 토큰 디코딩 기능 추가
    - `TokenPayload.ts`: JWT 페이로드 타입을 별도 관리
    - `protectedLoader`에서 `decodeToken()`을 사용하여 `userId` 추출
    - API에서 받아온 유저 데이터와 `userId`를 병합하여 반환

--- src/routers/protectedRoute.tsx
+++ src/routers/protectedRoute.tsx
@@ -1,22 +1,32 @@
 import { Outlet, redirect } from 'react-router-dom';
-import { getUser } from '@/apis/userApi';
 import { paths } from './paths';
+import { getUser } from '@/apis/userApi';
+import { decodeToken } from '@/utils/decodeToken';
+import { TokenPayload } from '@/types/tokenTypes';
 
 // TODO) 로그인 여부에 따라 루트 경로를 동적으로 리다이렉트
 // TODO) 로그인 상태에서는 (토큰 존재할 때) 퍼블릭 경로로 이동 불가하도록 설정
 export const protectedLoader = async () => {
+  const token = localStorage.getItem('AccessToken');
+
+  if (!token) {
+    return redirect(paths.auth.login.fullPath);
+  }
+
   try {
-    if (window.location.pathname === paths.auth.login.fullPath) {
-      return null; // getUser()를 실행하지 않도록 막음.
+    const decoded: TokenPayload | null = decodeToken(token);
+
+    if (!decoded) {
+      return redirect(paths.auth.login.fullPath);
     }
 
-    // api 호출
+    const userId = decoded.userId;
     const userResponse = await getUser();
-    return userResponse.data;
+
+    return { ...userResponse.data, userId };
   } catch (error) {
     return redirect(paths.auth.login.fullPath);
   }
-  return null;
 };
 
 const protectedRoute = () => {
