06a96245f350d415df54fab041b53c6f82fad1ca
Hyerim Yang <hlhlstar@naver.com>
2025-02-04 20:56:13 +0900

    feat: 프로필 수정 폼에 기존 사용자 데이터 바인딩 및 이미지 미리보기 추가
    - useForm을 활용해 프로필 수정 폼 구성
    - 기존 사용자 데이터를 defaultValues로 설정
    - 파일 선택 및 삭제 기능 추가
    - 업로드한 프로필 이미지를 미리보기(preview)로 표시

--- src/components/EditProfileInfo.tsx
+++ src/components/EditProfileInfo.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useGetUser } from '@/hooks/useUser';
 import Button from './Button';
 import Input from './Input';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
@@ -9,37 +12,86 @@ interface EditProfileInfoProps {
 }
 
 const EditProfileInfo = ({ onClose }: EditProfileInfoProps) => {
+  const { data: user } = useGetUser();
+  const { register, handleSubmit, setValue } = useForm({
+    defaultValues: {
+      username: user.username,
+      profileImage: user.profileImageUrl as string | File | null,
+    },
+  });
+
+  const [preview, setPreview] = useState<string | null>(user.profileImageUrl);
+
+  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const image = e.target.files?.[0];
+
+    if (image) {
+      setPreview(URL.createObjectURL(image));
+      setValue('profileImage', image);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setPreview(null);
+    setValue('profileImage', null);
+  };
+
+  // 프로필 수정
+  const onSubmit = (data: any) => {
+    console.log(data);
+    console.log(preview);
+
+    // 성공시 onClose
+  };
+
   return (
     <div className='div m-auto flex w-[300px] min-w-min flex-col gap-8 rounded-2xl bg-bg-deep p-4'>
-      {/* TODO) defalutValue로 기존 사진과 이름 가져오기 */}
       <form
         className='flex w-full flex-col items-center gap-y-4'
+        onSubmit={handleSubmit(onSubmit)}
         noValidate // 기본 HTML5 검증 메세지 제거
       >
         <div className='relative my-4 inline-block'>
           <Avatar className='h-32 w-32'>
-            <AvatarImage />
+            <AvatarImage
+              src={preview || ''}
+              className='h-full w-full object-cover'
+            />
             <AvatarFallback />
           </Avatar>
           <div className='absolute bottom-0 right-0 flex items-center divide-x-2 rounded-full bg-white shadow-md'>
-            <button className='flex items-center justify-center rounded-l-full p-2 hover:bg-gray-hover'>
+            <button
+              className='flex items-center justify-center rounded-l-full p-2 hover:bg-gray-hover'
+              type='button'
+              onClick={handleRemoveImage}
+            >
               <RiDeleteBin5Line />
             </button>
-            <button className='flex items-center justify-center rounded-r-full p-2 hover:bg-gray-hover'>
+            <label className='flex cursor-pointer items-center justify-center rounded-r-full p-2 hover:bg-gray-hover'>
               <RiUpload2Line />
-            </button>
+              <input
+                type='file'
+                accept='image/*'
+                {...register('profileImage')}
+                onChange={handleUploadImage}
+                className='hidden'
+              />
+            </label>
           </div>
         </div>
 
         <div className='flex w-full items-center gap-2'>
-          <label className='flex-none'>이름</label>
-          <Input />
+          <label htmlFor='username' className='flex-none'>
+            이름
+          </label>
+          <Input id='username' {...register('username', { required: true })} />
         </div>
 
         <div className='mt-4 flex min-w-full gap-4'>
           <Button
             children={'취소'}
             variant='outline'
+            type='button'
             onClick={onClose}
             className='flex-1 bg-bg'
           />
