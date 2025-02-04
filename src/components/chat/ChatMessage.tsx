26457e78a74f3641ebcf94e8f4247fa88f4ab7e1
kwonboryong <kwonboryong@naver.com>
2025-02-04 15:22:38 +0900

    feat: 읽음 표시를 위한 isRead 값 추가

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const ChatMessage = ({ nickname, chatting, time, isMe }: any) => {
+const ChatMessage = ({ nickname, chatting, time, isMe, isRead }: any) => {
   const [chatNick, setChatNick] = useState('');
 
   useEffect(() => {
@@ -10,30 +10,34 @@ const ChatMessage = ({ nickname, chatting, time, isMe }: any) => {
   return (
     <>
       {isMe ? (
-        <div id='me' className='my-5 flex justify-end'>
-          <div id='time' className='mr-3 flex flex-col justify-end'>
-            <p className='text-xs text-black'>{time}</p>
+        <div id='me' className='flex justify-end my-5'>
+          <div id='time' className='flex flex-col justify-end mr-3'>
+            <div className='flex gap-2'>
+              <p className='text-xs text-black'>{isRead}</p>
+              {/* {readBy.length > 0 && <span>읽음</span>} */}
+              <p className='text-xs text-black'>{time}</p>
+            </div>
           </div>
           <div className='mr-3'>
             <div id='nickname' className='flex justify-end'>
               <p className='text-xs text-black'>{chatNick}</p>
             </div>
-            <div id='chatting' className='mt-2 rounded-md bg-blue-500 p-3'>
+            <div id='chatting' className='p-3 mt-2 bg-blue-500 rounded-md'>
               <span className='text-xs text-white'>{chatting}</span>
             </div>
           </div>
         </div>
       ) : (
-        <div id='you' className='my-5 flex'>
+        <div id='you' className='flex my-5'>
           <div className='ml-3'>
             <p id='nickname' className='text-xs text-black'>
               {chatNick}
             </p>
-            <div id='chatting' className='mt-2 rounded-md bg-gray-200 p-3'>
+            <div id='chatting' className='p-3 mt-2 bg-gray-200 rounded-md'>
               <span className='text-xs text-black'>{chatting}</span>
             </div>
           </div>
-          <div id='time' className='ml-3 flex flex-col justify-end'>
+          <div id='time' className='flex flex-col justify-end ml-3'>
             <p className='text-xs text-black'>{time}</p>
           </div>
         </div>
