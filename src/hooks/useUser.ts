9a6932df40bd69eb4437fdda6b9f84269e228ae7
Hyerim Yang <hlhlstar@naver.com>
2025-02-04 15:57:27 +0900

    feat: protectedLoader에 React Query 적용 및 useGetUser에 initialData 설정
    - `protectedLoader.tsx`에서 `ensureQueryData`를 사용하여 유저 데이터를 캐싱하도록 적용
    - `useGetUser.ts`에서 `useLoaderData()`를 활용하여 `initialData` 설정
    - 첫 번째 렌더링에서 API 호출 없이 유저 데이터를 즉시 사용할 수 있도록 최적화

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,11 +1,17 @@
-import { getUserWithId } from '@/utils/getUserWithId';
+import { useLoaderData } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { UserData } from '@/types/userTypes';
+import { getUserWithId } from '@/utils/getUserWithId';
 
 // 사용자 정보 조회
 export const useGetUser = () => {
+  // protectedLoader()에서 반환한 값을 초기 데이터로 사용
+  const initialData = useLoaderData() as UserData;
+
   return useQuery({
     queryKey: ['user'],
     queryFn: getUserWithId,
+    initialData,
     retry: 3,
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 10,
