4fb6c57395fc3320db52c817cb82e7d068bfd2eb
kwonboryong <kwonboryong@naver.com>
2025-02-04 16:31:55 +0900

    fix: 유저 데이터 캐싱 방식 변경에 따른 유저 데이터 useGetQuery로 수정

--- src/pages/ProjectsIssueDetailPage.tsx
+++ src/pages/ProjectsIssueDetailPage.tsx
@@ -1,14 +1,10 @@
 import Chat from '../components/chat/Chat';
 
 const ProjectsIssueDetailPage = () => {
-  const { data } = useOutletContext<OutletContextType>();
-  console.log('user data:', data);
-  console.log('userID:', data.userId);
-
   return (
     <>
       <div>Issue Name | Issue ID</div>
-      <Chat userId={data.userId} username={data.username} />
+      <Chat />
     </>
   );
 };
