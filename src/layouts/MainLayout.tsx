43c9e375cca4d5dea507fb54625947d59327071c
Hyerim Yang <hlhlstar@naver.com>
2025-02-04 15:59:06 +0900

    refactor: MainLayout에서 useLoaderData 및 Outlet context 제거

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,13 @@
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navigation from '@/layouts/Navigation';
 import CustomModal from '@/components/CustomModal';
-import { UserData } from '@/types/userTypes';
 
-const MainLayout = ({}) => {
-  const data = useLoaderData() as UserData;
-  console.log('메인레이아웃', data);
-
+const MainLayout = () => {
   return (
     <div className='flex h-screen w-full flex-col'>
       <Navigation />
       <main className='flex h-full overflow-y-auto'>
-        <Outlet context={{ data }} />
+        <Outlet />
       </main>
 
       <CustomModal />
