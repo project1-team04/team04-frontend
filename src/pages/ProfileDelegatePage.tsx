1e39ff76deb92f5c4d0cfdac6faa4cfe2fd28696
kwonboryong <kwonboryong@naver.com>
2025-02-05 15:19:06 +0900

    feat: DelegateCard 컴포넌트에서 저장된 data(클릭한 유저 id, 프로젝트 id) 상위 페이지로 가져오기

--- src/pages/ProfileDelegatePage.tsx
+++ src/pages/ProfileDelegatePage.tsx
@@ -23,9 +23,17 @@ interface ProjectData {
   members: Member[];
 }
 
+interface DelegateSelection {
+  projectId: string;
+  userId: number;
+}
+
 const ProfileDelegatePage = () => {
   const navigate = useNavigate();
   const [projectsData, setProjects] = useState<ProjectData[]>([]);
+  const [selectedDelegates, setSelectedDelegates] = useState<
+    DelegateSelection[]
+  >([]);
 
   const fetchUsersProjects = async () => {
     try {
@@ -36,11 +44,24 @@ const ProfileDelegatePage = () => {
     }
   };
 
+  const handleSelectDelegate = (projectId: string, userId: number) => {
+    setSelectedDelegates((prev) => {
+      // 기존 배열에서 같은 projectId가 있으면 덮어씀(없으면 추가)
+      const updated = prev.filter((item) => item.projectId !== projectId);
+      return [...updated, { projectId, userId }];
+    });
+
+    console.log(`저장된 위임 정보:`, [
+      ...selectedDelegates,
+      { projectId, userId },
+    ]);
+  };
+
   const handleDelegateComplete = async () => {
     try {
-      // TODO: 위임 API 호출!!!!!!!!!!!
+      console.log('최종 위임 정보:', selectedDelegates);
 
-      navigate(paths.profile.root);
+      // navigate(paths.profile.root);
     } catch (error) {
       console.error('권한 위임 완료 에러', error);
     }
@@ -51,35 +72,36 @@ const ProfileDelegatePage = () => {
   }, []);
 
   return (
-    <div className='flex flex-col w-1/2 mx-auto'>
-      <header className='flex items-center gap-5 my-9'>
+    <div className='mx-auto flex w-1/2 flex-col'>
+      <header className='my-9 flex items-center gap-5'>
         <Header children={'프로젝트 권한 위임'} />
       </header>
 
-      <main className='overflow-hidden mb-9 grow'>
-        <div className='flex flex-col items-center h-full p-4 rounded-2xl bg-bg-deep'>
-          <div className='flex flex-col flex-1 w-full gap-4 overflow-y-auto'>
+      <main className='mb-9 grow overflow-hidden'>
+        <div className='flex h-full flex-col items-center rounded-2xl bg-bg-deep p-4'>
+          <div className='flex w-full flex-1 flex-col gap-4 overflow-y-auto'>
             {projectsData.map((projectData) => (
               <DelegateCard
                 key={projectData.project.id}
                 project={projectData.project}
                 members={projectData.members}
+                onSelectDelegate={handleSelectDelegate}
               />
             ))}
           </div>
 
-          <div className='flex w-2/3 gap-4 mt-4'>
+          <div className='mt-4 flex w-2/3 gap-4'>
             <Button
               children={'취소'}
               variant='outline'
-              className='flex-1 border border-default bg-bg'
+              className='border-default flex-1 border bg-bg'
               onClick={() => navigate(paths.profile.root)}
             />
-            {/* TODO) onClick 이벤트 핸들러에서 api 호출 후 paths.profile.root로 navigate */}
             <Button
               children={'위임 완료'}
               className='flex-1'
               onClick={handleDelegateComplete}
+              disabled={Object.keys(selectedDelegates).length === 0}
             />
           </div>
         </div>
