b8ad0b34b8547a131c9088e2a00bb32492f9f059
kwonboryong <kwonboryong@naver.com>
2025-02-05 13:54:35 +0900

    feat: 프로젝트 권한 위임 페이지 구현 - 목 데이터 삭제 - 유저가 PM인 프로젝트들과 멤버 조회 api 생성 및 추가 - api 반환 값에 따른 기존 interface 개선 - 반환 값에 따른 DelegateCard 동적 생성 - DelegateCard에 넘겨주는 props 수정

--- src/pages/ProfileDelegatePage.tsx
+++ src/pages/ProfileDelegatePage.tsx
@@ -3,48 +3,84 @@ import { paths } from '@/routers/paths';
 import Button from '@/components/Button';
 import Header from '@/components/Header';
 import DelegateCard from '@/components/DelegateCard';
+import { getProjectsWithMembersByManager } from '@/apis/projectApi';
+import { useEffect, useState } from 'react';
 
-// 목 데이터
-const data = [
-  { id: '1', title: 'Threadly', member: ['앵무새', '고양이', '강아지'] },
-  { id: '2', title: 'Apple', member: ['권', '보', '령'] },
-  { id: '3', title: 'Banana', member: ['ㅇ', 'ㄴ', 'ㅈ'] },
-  // { id: '33', title: 'Banana', member: ['ㅇ', 'ㄴ', 'ㅈ'] },
-  // { id: '31', title: 'Banana', member: ['ㅇ', 'ㄴ', 'ㅈ'] },
-  // { id: '321', title: 'Banana', member: ['ㅇ', 'ㄴ', 'ㅈ'] },
-];
+interface Member {
+  userId: number;
+  userName: string;
+  email: string;
+  role: string;
+}
+
+interface ProjectData {
+  project: {
+    id: string;
+    projectKey: string;
+    name: string;
+    issueCount: number;
+  };
+  members: Member[];
+}
 
 const ProfileDelegatePage = () => {
   const navigate = useNavigate();
+  const [projectsData, setProjects] = useState<ProjectData[]>([]);
+
+  const fetchUsersProjects = async () => {
+    try {
+      const res = await getProjectsWithMembersByManager();
+      setProjects(res);
+    } catch (error) {
+      console.log('프로젝트 권한 위임 에러', error);
+    }
+  };
+
+  const handleDelegateComplete = async () => {
+    try {
+      // TODO: 위임 API 호출!!!!!!!!!!!
+
+      navigate(paths.profile.root);
+    } catch (error) {
+      console.error('권한 위임 완료 에러', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsersProjects();
+  }, []);
 
   return (
-    <div className='mx-auto flex w-1/2 flex-col'>
-      <header className='my-9 flex items-center gap-5'>
+    <div className='flex flex-col w-1/2 mx-auto'>
+      <header className='flex items-center gap-5 my-9'>
         <Header children={'프로젝트 권한 위임'} />
       </header>
 
-      <main className='mb-9 grow overflow-hidden'>
-        <div className='flex h-full flex-col items-center rounded-2xl bg-bg-deep p-4'>
-          {/* 스크롤 적용 영역 */}
-          <div className='flex w-full flex-1 flex-col gap-4 overflow-y-auto'>
-            {data.map((project) => (
+      <main className='overflow-hidden mb-9 grow'>
+        <div className='flex flex-col items-center h-full p-4 rounded-2xl bg-bg-deep'>
+          <div className='flex flex-col flex-1 w-full gap-4 overflow-y-auto'>
+            {projectsData.map((projectData) => (
               <DelegateCard
-                key={project.id}
-                title={project.title}
-                member={project.member}
+                key={projectData.project.id}
+                project={projectData.project}
+                members={projectData.members}
               />
             ))}
           </div>
 
-          <div className='mt-4 flex w-2/3 gap-4'>
+          <div className='flex w-2/3 gap-4 mt-4'>
             <Button
               children={'취소'}
               variant='outline'
-              className='border-default flex-1 border bg-bg'
+              className='flex-1 border border-default bg-bg'
               onClick={() => navigate(paths.profile.root)}
             />
             {/* TODO) onClick 이벤트 핸들러에서 api 호출 후 paths.profile.root로 navigate */}
-            <Button children={'위임 완료'} className='flex-1' />
+            <Button
+              children={'위임 완료'}
+              className='flex-1'
+              onClick={handleDelegateComplete}
+            />
           </div>
         </div>
       </main>
