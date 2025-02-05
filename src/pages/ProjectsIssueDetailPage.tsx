1dc0a3430673bc52e0bee173cd7c08be5fbb4d66
Hyerim Yang <hlhlstar@naver.com>
2025-02-05 22:25:58 +0900

    feat: 이슈 삭제 전 경고 모달 추가
    - 삭제 버튼 클릭 시 '이슈 삭제 여부'를 묻는 모달이 나타나도록 변경
    - 모달에서 '삭제' 버튼을 눌러야 실제 삭제가 진행되도록 수정

--- src/pages/ProjectsIssueDetailPage.tsx
+++ src/pages/ProjectsIssueDetailPage.tsx
@@ -2,11 +2,13 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useDeleteIssue, useGetIssue } from '@/hooks/useIssue';
 import Button from '@/components/Button';
 import Chat from '@/components/chat/Chat';
-import { IoChevronBack } from 'react-icons/io5';
+import { IoChevronBack, IoWarning } from 'react-icons/io5';
+import { useCustomModalStore } from '@/stores/useCustomModalStore';
 
 const ProjectsIssueDetailPage = () => {
   const navigate = useNavigate();
   const { projectId, issueId } = useParams();
+  const showModal = useCustomModalStore((state) => state.showModal);
   const { mutate: deleteIssueMutate, isPending: isDeleting } = useDeleteIssue();
 
   // 이슈 조회 API 호출
@@ -43,7 +45,26 @@ const ProjectsIssueDetailPage = () => {
             variant='negative'
             size='sm'
             className='w-fit'
-            onClick={handleDeleteIssue}
+            onClick={() =>
+              showModal({
+                icon: <IoWarning size={40} className='text-red' />,
+                title: '이슈를 삭제하시겠습니까?',
+                content: '삭제한 이슈는 복구되지 않습니다.',
+                buttons: [
+                  {
+                    text: '취소',
+                    variant: 'outline',
+                    disabled: isDeleting,
+                  },
+                  {
+                    text: '삭제',
+                    variant: 'negative',
+                    onClick: handleDeleteIssue,
+                    disabled: isDeleting,
+                  },
+                ],
+              })
+            }
             disabled={isDeleting}
           />
         </div>
