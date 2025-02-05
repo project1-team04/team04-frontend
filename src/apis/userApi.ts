4ce1f65db4d1e3b5732b4843eb75f1339ea65d8a
Hyerim Yang <hlhlstar@naver.com>
2025-02-05 10:40:40 +0900

    feat: 사용자 정보 업데이트 훅 및 API 함수 구현
    - updateUser API 함수 추가
    - useUpdateUser 훅 추가 (React Query의 useMutation 사용)
    - 사용자 정보 변경 시 캐시 무효화 처리

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -3,3 +3,11 @@ import instance from './instance';
 export const getUser = () => {
   return instance.get('user/profile');
 };
+
+export const updateUser = (data: FormData) => {
+  return instance.patch('user/profile', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
