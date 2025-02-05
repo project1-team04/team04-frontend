646b05efcdc64ff6d0ee30e53dae2408421baefd
kwonboryong <kwonboryong@naver.com>
2025-02-05 20:58:25 +0900

    feat: 채팅 프로필 컴포넌트에 데이터 추가

--- src/components/chat/ChatProfile.tsx
+++ src/components/chat/ChatProfile.tsx
@@ -1,8 +1,18 @@
-const ChatProfile = () => {
+const ChatProfile = ({
+  userName,
+  imgUrl,
+}: {
+  userName: string;
+  imgUrl: string | null;
+}) => {
   return (
     <img
-      src='https://www.zespri.com/content/dam/zespri/kr/kiwibrother/about_gold_figure.jpeg'
-      alt=''
+      src={
+        imgUrl ||
+        'https://www.zespri.com/content/dam/zespri/kr/kiwibrother/about_gold_figure.jpeg'
+      }
+      alt={`${userName}의 프로필`}
+      title={userName}
       className='h-10 w-10 rounded-full border-[1px] border-border-default'
     />
   );
